73c79bcca0e82cb510b161fe669b5eafcda82096
Rohit Gupta <rohit.gupta@eurecom.fr>
2016-02-05 10:49:14 +0100

    stop Gitlab CI execution when cleanup fails

--- cmake_targets/autotests/run_exec_lte-softmodem_tests.py
+++ cmake_targets/autotests/run_exec_lte-softmodem_tests.py
@@ -1015,6 +1015,8 @@ class oaiCleanOldProgramThread (threading.Thread):
            error = error + '\n threadID = ' + str(self.threadID) + '\n threadname = ' + self.threadname + '\n CleanUpOldProgs = ' + self.CleanUpOldProgs + '\n CleanUpAluLteBox = ' + self.CleanUpAluLteBox + '\n ExmimoRfStop = ' + self.ExmimoRfStop + '\n'  
            error = error + traceback.format_exc()
            print error
+           print "There is error in cleaning up old programs. The test case execution cannot continue...."
+           sys.exit(1)
 
 # \brief Run parallel threads in all machines for clean up old execution of test cases
 # \param oai_list list of handlers that can be used to execute programs on remote machines
