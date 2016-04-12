20e2460913999148b15a3130c223fceabd72b6ea
gauthier <lionel.gauthier@eurecom.fr>
2016-04-12 12:38:19 +0200

    minor change in timing for 5 MHz on USRP B210

--- targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
+++ targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp
@@ -627,7 +627,7 @@ extern "C" {
     case 7680000:
       s->usrp->set_master_clock_rate(7.68e6);
       openair0_cfg[0].samples_per_packet    = 1024;
-      openair0_cfg[0].tx_sample_advance     = 70;//103;
+      openair0_cfg[0].tx_sample_advance     = 103;
       openair0_cfg[0].tx_bw                 = 5e6;
       openair0_cfg[0].rx_bw                 = 5e6;
       openair0_cfg[0].tx_scheduling_advance = 5*openair0_cfg[0].samples_per_packet;
