bd50539fda96214ac1a35f5471e4872cdeb05b2d
Cedric Roux <cedric.roux@eurecom.fr>
2016-05-01 14:13:15 +0200

    Revert "instrument X using gcc's -finstrument-functions"
    This reverts commit 4911b107dae26150ec87cc31cada0e52f97061d6.
    
    The idea was to keep the stuff in the git history.

--- common/utils/T/tracer/gui/gui.c
+++ common/utils/T/tracer/gui/gui.c
@@ -5,31 +5,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-static volatile int locked = 0;
-
-void __cyg_profile_func_enter (void *func,  void *caller)
-{
-  if (locked == 0) abort();
-  printf("E %p %p %lu\n", func, caller, time(NULL));
-}
-
-void __cyg_profile_func_exit (void *func, void *caller)
-{
-  if (locked == 0) abort();
-  printf("X %p %p %lu\n", func, caller, time(NULL));
-}
-
 void glock(gui *_gui)
 {
   struct gui *g = _gui;
   if (pthread_mutex_lock(g->lock)) ERR("mutex error\n");
-  locked = 1;
 }
 
 void gunlock(gui *_gui)
 {
   struct gui *g = _gui;
-  locked = 0;
   if (pthread_mutex_unlock(g->lock)) ERR("mutex error\n");
 }
 
