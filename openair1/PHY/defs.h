36df9556c287ad6c67384bf7930a0c8480a9295c
Sandeep Kumar <ee13b1025@iith.ac.in>
2016-06-27 17:26:43 +0200

    minor typo

--- openair1/PHY/defs.h
+++ openair1/PHY/defs.h
@@ -161,7 +161,7 @@ typedef enum {
   eNodeB_3GPP=0,    // classical eNodeB function
   eNodeB_3GPP_BBU,  // classical eNodeB function with transport interface
   NGFI_RRU_IF4,     // NGFI_RRU (NGFI remote radio-unit, currently split at common - ue_specific interface, IF4) 
-  NGFI_RCC_IF4,     // NGFI_RCC (NGFI radio cloud center, currently split at common - ue_specific interface, IF4) 
+  NGFI_RCC_IF4     // NGFI_RCC (NGFI radio cloud center, currently split at common - ue_specific interface, IF4) 
 } eNB_func_t;
   
 typedef struct UE_SCAN_INFO_s {
