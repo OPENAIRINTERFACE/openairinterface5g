f99e5e1ff068f7ac41325c70342c42137f739e29
Frédéric Leroy <frederic.leroy@b-com.com>
2016-07-12 12:54:40 +0200

    UE/ESM: remove useless #if directive in esm_ebr.c

--- openair3/NAS/UE/ESM/esm_ebr.c
+++ openair3/NAS/UE/ESM/esm_ebr.c
@@ -69,9 +69,7 @@ static const char *_esm_ebr_state_str[ESM_EBR_STATE_MAX] = {
  * ----------------------------------
  */
 
-#if !defined(NAS_BUILT_IN_EPC)
 static esm_ebr_data_t _esm_ebr_data[ESM_EBR_NB_UE_MAX];
-#endif
 
 /*
  * ----------------------
@@ -126,7 +124,6 @@ void esm_ebr_initialize(
   esm_indication_callback_t cb
 )
 {
-#if !defined(NAS_BUILT_IN_EPC)
   int ueid, i;
 
   LOG_FUNC_IN;
@@ -144,7 +141,6 @@ void esm_ebr_initialize(
   _esm_ebr_callback = *cb;
 
   LOG_FUNC_OUT;
-#endif
 }
 
 /****************************************************************************
