f764aea62d9bcde8b80af819bf150100e82bd8b2
Rohit Gupta <rohit.gupta@eurecom.fr>
2016-08-23 22:46:03 +0200

    logging for killing old programs

--- cmake_targets/autotests/tools/remove_old_programs.bash
+++ cmake_targets/autotests/tools/remove_old_programs.bash
@@ -3,6 +3,9 @@
 #$1 programs to be killed and checked
 echo "removing old programs..."
 echo "args = $1"
+echo "programs to be killed"
+ps -aux |grep -E -i $1
+
 ps -aux |grep -E -i $1| awk '{print $2}' | sudo xargs kill -9 
 
 var=`ps -aux |grep -E -i $1`
