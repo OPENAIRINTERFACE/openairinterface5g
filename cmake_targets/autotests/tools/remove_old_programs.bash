d65c4a25a33ea02954511af83d7c3ce58d6d0630
Rohit Gupta <rohit.gupta@eurecom.fr>
2016-08-24 00:01:14 +0200

    bugfix for script to kill old programs

--- cmake_targets/autotests/tools/remove_old_programs.bash
+++ cmake_targets/autotests/tools/remove_old_programs.bash
@@ -3,12 +3,19 @@
 #$1 programs to be killed and checked
 echo "removing old programs..."
 echo "args = $1"
+echo "script name = $0"
+filename=$(basename "$0")
 echo "programs to be killed"
-ps -aux |grep -E -i $1
+echo "bash PID = $$"
+pid='$$'
+#we need to remove current program and grip as we kill ourselves otherwise :)
+var=`ps -aux |grep -E -i $1 | awk '{print $2}'`
 
-ps -aux |grep -E -i $1| awk '{print $2}' | sudo xargs kill -9 
+echo $var
 
-var=`ps -aux |grep -E -i $1`
+echo "$var" | sed 's/'$$'/ /' | sudo xargs kill -9
+
+var=`ps -aux |grep -E -i $1| grep -E -v '$filename|grep|$$'`
 echo $var
 if [ -n "$var" ]; then echo 'Match found'; else echo 'Match not found' ;fi 
 
