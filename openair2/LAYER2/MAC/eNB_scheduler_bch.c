754cab7ec44631db4bc194829e0880e1954c2b1b
Wilson W.K. Thong <wilsonthong@astri.org>
2016-09-19 15:08:50 +0800

    added SFN to wireshark logging output
    Conflicts:
    	openair2/LAYER2/MAC/eNB_scheduler_ulsch.c
    	openair2/LAYER2/MAC/ue_procedures.c

--- openair2/LAYER2/MAC/eNB_scheduler_bch.c
+++ openair2/LAYER2/MAC/eNB_scheduler_bch.c
@@ -319,6 +319,7 @@ schedule_SI(
                   0xffff,
                   4,
                   0xffff,
+                  eNB->frame,
                   eNB->subframe,
                   0,
                   0);
