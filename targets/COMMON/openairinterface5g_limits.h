1cd46a75d726a52b6557357a50c1c896620a9fc7
Cedric Roux <cedric.roux@eurecom.fr>
2017-01-27 15:49:00 +0100

    add missing #ifndef #endif guards in header file
    	modified:   targets/COMMON/openairinterface5g_limits.h

--- targets/COMMON/openairinterface5g_limits.h
+++ targets/COMMON/openairinterface5g_limits.h
@@ -1,3 +1,6 @@
+#ifndef OPENAIRINTERFACE5G_LIMITS_H_
+#define OPENAIRINTERFACE5G_LIMITS_H_
+
 #if defined(CBMIMO1) || defined(EXMIMO) || defined(OAI_USRP)
 	#define NUMBER_OF_eNB_MAX 1
 	#define NUMBER_OF_UE_MAX 16
@@ -17,4 +20,6 @@
 		#define NUMBER_OF_UE_MAX 120
 		#define NUMBER_OF_CONNECTED_eNB_MAX 1 // to save some memory
 	#endif
+#endif
+
 #endif
