924c421beb3d6a8833290a711d05114d2071ae63
Wilson Thong <wilsonthong@astri.org>
2017-03-28 10:17:42 +0800

    #220 add help printout for run options

--- targets/RT/USER/lte-softmodem.c
+++ targets/RT/USER/lte-softmodem.c
@@ -329,6 +329,7 @@ void help (void) {
   printf("  --external-clock tells hardware to use an external clock reference\n");
   printf("  --usim-test use XOR autentication algo in case of test usim mode\n"); 
   printf("  --single-thread-disable. Disables single-thread mode in lte-softmodem\n"); 
+  printf("  -A Set timing_advance\n");
   printf("  -C Set the downlink frequency for all component carriers\n");
   printf("  -d Enable soft scope and L1 and L2 stats (Xforms)\n");
   printf("  -F Calibrate the EXMIMO borad, available files: exmimo2_2arxg.lime exmimo2_2brxg.lime \n");
