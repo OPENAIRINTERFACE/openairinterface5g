c56b5af00e1e1d402d094d03db7011097e738ae7
Robert Schmidt <robert.schmidt@eurecom.fr>
2018-04-20 08:45:44 +0200

    bugfix: kill td/te threads only if initialized
    * init_td_thread()/init_te_thread() are only initialized depending on condition
      get_nprocs() > 2 && codingw
    * apply the same the same for the functions kill_td_thread()/kill_te_thread()

--- targets/RT/USER/lte-enb.c
+++ targets/RT/USER/lte-enb.c
@@ -1031,8 +1031,10 @@ void kill_eNB_proc(int inst) {
     proc = &eNB->proc;
     proc_rxtx = &proc->proc_rxtx[0];
 
-    kill_td_thread(eNB);
-    kill_te_thread(eNB);
+    if(get_nprocs() > 2 && codingw) {
+      kill_td_thread(eNB);
+      kill_te_thread(eNB);
+    }
     LOG_I(PHY, "Killing TX CC_id %d inst %d\n", CC_id, inst );
     for (i=0; i<2; i++) {
       pthread_mutex_lock(&proc_rxtx[i].mutex_rxtx);
