9d82d5aebd31d0a4d08a85e3e448451d359fba15
Raymond Knopp <raymond.knopp@eurecom.fr>
2018-05-04 15:55:32 +0800

    added command line parameter to set external timing for PPS on USRP.

--- targets/RT/USER/lte-ru.c
+++ targets/RT/USER/lte-ru.c
@@ -1540,7 +1540,7 @@ static void* ru_stats_thread(void* param) {
   while (!oai_exit) {
     sleep(1);
     if (opp_enabled == 1) {
-      print_meas(&proc->ru_arrival_time,"ru_arrival_time",NULL,NULL);
+     // print_meas(&proc->ru_arrival_time,"ru_arrival_time",NULL,NULL);
       if (ru->feprx) print_meas(&ru->ofdm_demod_stats,"feprx",NULL,NULL);
       if (ru->feptx_ofdm) print_meas(&ru->ofdm_mod_stats,"feptx_ofdm",NULL,NULL);
       if (ru->fh_north_asynch_in) print_meas(&ru->rx_fhaul,"rx_fhaul",NULL,NULL);
--- targets/RT/USER/lte-softmodem.h
+++ targets/RT/USER/lte-softmodem.h
@@ -63,6 +63,7 @@
 #define CONFIG_HLP_PHYTST        "test UE phy layer, mac disabled\n"
 #define CONFIG_HLP_DMAMAP        "sets flag for improved EXMIMO UE performance\n"  
 #define CONFIG_HLP_EXCCLK        "tells hardware to use an external clock reference\n"
+#define CONFIG_HLP_EXTS          "tells hardware to use an external timing reference\n"
 #define CONFIG_HLP_USIM          "use XOR autentication algo in case of test usim mode\n" 
 #define CONFIG_HLP_NOSNGLT       "Disables single-thread mode in lte-softmodem\n" 
 #define CONFIG_HLP_TADV          "Set timing_advance\n"
@@ -145,6 +146,7 @@ extern int16_t dlsch_demod_shift;
 {"usim-test",             	 CONFIG_HLP_USIM,	PARAMFLAG_BOOL,   u8ptr:&usim_test,			defintval:0,			   TYPE_UINT8,    0},			   \
 {"mmapped-dma",           	 CONFIG_HLP_DMAMAP,	PARAMFLAG_BOOL,   uptr:&mmapped_dma,			defintval:0,			   TYPE_INT,	  0},			   \
 {"external-clock",        	 CONFIG_HLP_EXCCLK,	PARAMFLAG_BOOL,   uptr:&clock_source,			defintval:0,			   TYPE_INT,	  0},			   \
+{"external-timesource",          CONFIG_HLP_EXTS,       PARAMFLAG_BOOL,   uptr:&time_source,                    defintval:0,                       TYPE_INT,      0},                      \
 {"wait-for-sync",         	 NULL,  		PARAMFLAG_BOOL,   iptr:&wait_for_sync,  		defintval:0,			   TYPE_INT,	  0},			   \
 {"single-thread-disable", 	 CONFIG_HLP_NOSNGLT,	PARAMFLAG_BOOL,   iptr:&single_thread_flag,		defintval:1,			   TYPE_INT,	  0},			   \
 {"threadIQ",              	 NULL,  		0,		  iptr:&(threads.iq),			defintval:1,			   TYPE_INT,	  0},			   \
