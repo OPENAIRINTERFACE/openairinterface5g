93f494f10655a6c27d8e7d9f756f6da5fe68d528
Thomas Schlichter <thomas.schlichter@iis.fraunhofer.de>
2018-05-17 12:21:39 +0200

    Fix channel impulse response peak detection when adjusting the timing sync
    This commit was developed at Fraunhofer IIS (https://www.iis.fraunhofer.de).

--- openair1/PHY/LTE_ESTIMATION/lte_adjust_sync.c
+++ openair1/PHY/LTE_ESTIMATION/lte_adjust_sync.c
@@ -61,8 +61,8 @@ void lte_adjust_synch(LTE_DL_FRAME_PARMS *frame_parms,
     temp = 0;
 
     for (aa=0; aa<frame_parms->nb_antennas_rx; aa++) {
-      Re = ((int16_t*)ue->common_vars.common_vars_rx_data_per_thread[ue->current_thread_id[subframe]].dl_ch_estimates_time[eNB_id][aa])[(i<<2)];
-      Im = ((int16_t*)ue->common_vars.common_vars_rx_data_per_thread[ue->current_thread_id[subframe]].dl_ch_estimates_time[eNB_id][aa])[1+(i<<2)];
+      Re = ((int16_t*)ue->common_vars.common_vars_rx_data_per_thread[ue->current_thread_id[subframe]].dl_ch_estimates_time[eNB_id][aa])[(i<<1)];
+      Im = ((int16_t*)ue->common_vars.common_vars_rx_data_per_thread[ue->current_thread_id[subframe]].dl_ch_estimates_time[eNB_id][aa])[1+(i<<1)];
       temp += (Re*Re/2) + (Im*Im/2);
     }
 
