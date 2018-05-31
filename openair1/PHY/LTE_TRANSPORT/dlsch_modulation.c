7e00727ce744f78e094f0b7ee020bb136478a8ef
Wang Tsu-Han <wangts@eurecom.fr>
2018-05-31 14:27:54 +0200

    using accelarated modulation

--- openair1/PHY/LTE_TRANSPORT/dlsch_modulation.c
+++ openair1/PHY/LTE_TRANSPORT/dlsch_modulation.c
@@ -2598,7 +2598,7 @@ int dlsch_modulation(PHY_VARS_eNB* phy_vars_eNB,
                                rb);
 
 
-      allocate_REs_in_RB(phy_vars_eNB,
+      allocate_REs(phy_vars_eNB,
                          txdataF,
                          &jj,
                          &jj2,
