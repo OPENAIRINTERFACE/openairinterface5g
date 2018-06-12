1a039eecbd021c4a886f00e7ab8d0ca413d4e50c
Hongzhi Wang <hongzhi.wang@tcl.com>
2018-06-12 17:36:45 +0200

    ue missing files

---
+++ openair1/PHY/NR_REFSIG/refsig_defs_ue.h
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+/* Definitions for LTE Reference signals */
+/* Author R. Knopp / EURECOM / OpenAirInterface.org */
+#ifndef __NR_REFSIG_DEFS__H__
+#define __NR_REFSIG_DEFS__H__
+#include "PHY/defs_nr_UE.h"
+
+
+/*!\brief This function generates the NR Gold sequence (38-211, Sec 5.2.1) for the PBCH DMRS.
+@param PHY_VARS_NR_UE* ue structure provides configuration, frame parameters and the pointers to the 32 bits sequence storage tables
+ */
+int nr_pbch_dmrs_rx(unsigned int *nr_gold_pbch,	int32_t *output	);
+
+/*int nr_pdsch_dmrs_rx(PHY_VARS_NR_UE *ue,
+						unsigned int ncp,
+						unsigned int Ns,
+						unsigned int nr_gold_pdsch[2][20][2][21],
+						int32_t *output,
+						unsigned short p,
+						int length_dmrs,
+						unsigned short nb_pdsch_rb);*/
+
+#endif
