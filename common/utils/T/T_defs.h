d89009becfdda3cfeea818486052b93f6de21afd
Cedric Roux <cedric.roux@eurecom.fr>
2018-07-20 16:04:39 +0200

    fix

--- common/utils/T/T_defs.h
+++ common/utils/T/T_defs.h
@@ -39,10 +39,10 @@ typedef struct {
 #define T_SHM_FILENAME "/T_shm_segment"
 
 /* number of VCD functions (to be kept up to date! see in T_messages.txt) */
-#define VCD_NUM_FUNCTIONS 187
+#define VCD_NUM_FUNCTIONS 190
 
 /* number of VCD variables (to be kept up to date! see in T_messages.txt) */
-#define VCD_NUM_VARIABLES 131
+#define VCD_NUM_VARIABLES 128
 
 /* first VCD function (to be kept up to date! see in T_messages.txt) */
 #define VCD_FIRST_FUNCTION    ((uintptr_t)T_VCD_FUNCTION_RT_SLEEP)
