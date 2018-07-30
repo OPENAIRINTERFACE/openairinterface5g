5798b177371dac5c914adf4579fb67003dfa200a
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2018-07-30 15:12:26 +0200

    Dummy push to trigger CI again
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- openair1/PHY/TOOLS/lte_dfts.c
+++ openair1/PHY/TOOLS/lte_dfts.c
@@ -49,9 +49,7 @@ int oai_exit=0;
 
 #include "PHY/sse_intrin.h"
 
-
 #include "assertions.h"
-
 
 #define print_shorts(s,x) printf("%s %d,%d,%d,%d,%d,%d,%d,%d\n",s,(x)[0],(x)[1],(x)[2],(x)[3],(x)[4],(x)[5],(x)[6],(x)[7])
 #define print_shorts256(s,x) printf("%s %d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",s,(x)[0],(x)[1],(x)[2],(x)[3],(x)[4],(x)[5],(x)[6],(x)[7],(x)[8],(x)[9],(x)[10],(x)[11],(x)[12],(x)[13],(x)[14],(x)[15])
