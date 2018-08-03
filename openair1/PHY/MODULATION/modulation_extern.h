4221816f5eab6652bb97456b8add8d5f084c9236
laurent <laurent.thomas@open-cells.com>
2018-08-03 20:28:34 +0200

    generate 7.5KHz tables

--- openair1/PHY/MODULATION/modulation_extern.h
+++ openair1/PHY/MODULATION/modulation_extern.h
@@ -19,18 +19,18 @@
  *      contact@openairinterface.org
  */
 
-extern int16_t s6n_kHz_7_5[1920];
-extern int16_t s6e_kHz_7_5[1920];
-extern int16_t s15n_kHz_7_5[3840];
-extern int16_t s15e_kHz_7_5[3840];
-extern int16_t s25n_kHz_7_5[7680];
-extern int16_t s25e_kHz_7_5[7680];
-extern int16_t s50n_kHz_7_5[15360];
-extern int16_t s50e_kHz_7_5[15360];
-extern int16_t s75n_kHz_7_5[23040];
-extern int16_t s75e_kHz_7_5[23040];
-extern int16_t s100n_kHz_7_5[30720];
-extern int16_t s100e_kHz_7_5[30720];
+extern int16_t *s6n_kHz_7_5;
+extern int16_t *s6e_kHz_7_5;
+extern int16_t *s15n_kHz_7_5;
+extern int16_t *s15e_kHz_7_5;
+extern int16_t *s25n_kHz_7_5;
+extern int16_t *s25e_kHz_7_5;
+extern int16_t *s50n_kHz_7_5;
+extern int16_t *s50e_kHz_7_5;
+extern int16_t *s75n_kHz_7_5;
+extern int16_t *s75e_kHz_7_5;
+extern int16_t *s100n_kHz_7_5;
+extern int16_t *s100e_kHz_7_5;
 
 
 extern short conjugate75[8];
