9cb461ec75343c5f43c07da3ea0a68528f0fbd03
Raymond Knopp <raymond.knopp@eurecom.fr>
2019-02-17 22:25:40 +0100

    reduced computations for TX during S subframe

--- openair1/SCHED/ru_procedures.c
+++ openair1/SCHED/ru_procedures.c
@@ -95,7 +95,10 @@ void feptx0(RU_t *ru,int slot) {
 		  "ru->generate_dmrs_sync should not be set, frame_type %d, is_slave %d\n",
 		  fp->frame_type,ru->is_slave);
 */
+      int num_symb = 7;
 
+      if (subframe_select(fp,subframe) == SF_S) num_symb=fp->dl_symbols_in_S_subframe;
+   
       if (ru->generate_dmrs_sync == 1 && slot == 0 && subframe == 1 && aa==0) {
 	//int32_t dmrs[ru->frame_parms.ofdm_symbol_size*14] __attribute__((aligned(32)));
         //int32_t *dmrsp[2] ={dmrs,NULL}; //{&dmrs[(3-ru->frame_parms.Ncp)*ru->frame_parms.ofdm_symbol_size],NULL};
@@ -113,7 +116,7 @@ void feptx0(RU_t *ru,int slot) {
       } 
       normal_prefix_mod(&ru->common.txdataF_BF[aa][slot*slot_sizeF],
                         (int*)&ru->common.txdata[aa][slot_offset],
-                        7,
+                        num_symb,
                         fp);
 
        
