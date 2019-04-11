a3f2ce1b2cc26ab95a791c726dbfeb9af2095431
Javier Morgade <javier.morgade@ieee.org>
2019-04-11 21:32:32 +0200

    VCD FeMBMS Prototypes

--- common/utils/T/T_defs.h
+++ common/utils/T/T_defs.h
@@ -41,7 +41,7 @@ typedef struct {
 } T_cache_t;
 
 /* number of VCD functions (to be kept up to date! see in T_messages.txt) */
-#define VCD_NUM_FUNCTIONS 190
+#define VCD_NUM_FUNCTIONS 195
 
 /* number of VCD variables (to be kept up to date! see in T_messages.txt) */
 #define VCD_NUM_VARIABLES 128
