db9e181d407bf0da3cc3ba8f3d0d7398a249d2a0
Boris Djalal <boris.djalal@eurecom.fr>
2019-05-07 16:24:54 +0200

    CI: adding support for the N300 gNB + NR-UE testing   -- pipeline is polling the develop-nr branch   -- new conf file / new scenario files   -- main python now supports LTE and 5G-NR executables in a Ubuntu/CentOS env

--- targets/RT/USER/nr-softmodem.c
+++ targets/RT/USER/nr-softmodem.c
@@ -1237,6 +1237,8 @@ int main( int argc, char **argv )
 
   // wait for end of program
   printf("TYPE <CTRL-C> TO TERMINATE\n");
+  fflush(stdout);
+  fflush(stderr);
   //getchar();
 
 #if defined(ENABLE_ITTI)
