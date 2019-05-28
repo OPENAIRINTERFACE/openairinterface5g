2d3d0f7ae7029e6c99ff5e6df980f5c6f1674d34
Dong Anyuan <donganyuan@cn.fujitsu.com>
2019-05-28 22:21:04 +0900

    Fix Coverity Scan CID 300396 (Variable mac_MainConfig going out of scope leaks the storage it points to.)

--- openair2/RRC/LTE/rrc_eNB.c
+++ openair2/RRC/LTE/rrc_eNB.c
@@ -3927,6 +3927,19 @@ flexran_rrc_eNB_generate_defaultRRCConnectionReconfiguration(const protocol_ctxt
     }
     free(quantityConfig);
   }
+
+  if(mac_MainConfig){
+      if(mac_MainConfig->ul_SCH_Config){
+          free(mac_MainConfig->ul_SCH_Config);
+          mac_MainConfig->ul_SCH_Config = NULL;
+      }
+      if(mac_MainConfig->phr_Config){
+          free(mac_MainConfig->phr_Config);
+          mac_MainConfig->phr_Config = NULL;
+      }
+      free(mac_MainConfig);
+      mac_MainConfig = NULL;
+  }
 }
 
 
