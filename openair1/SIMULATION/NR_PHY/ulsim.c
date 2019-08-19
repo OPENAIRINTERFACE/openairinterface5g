f398e4999bb4d9ca7aead5f24d75eaa5bee23d94
Ahmed Hussein <ahmed.hussein@iis.fraunhofer.de>
2019-08-19 17:05:18 +0200

    Set start_rb to 0
    - start_rb was set to 30, and nb_rb was set to 50 with NB_RB_DL = 106.
    - When the DC subcarrier is included in the RB allocation,
      this causes errors in FFT/IFFT operations arounf the DC subcarrier
    - This leads to errors in unscrambling and decoding

--- openair1/SIMULATION/NR_PHY/ulsim.c
+++ openair1/SIMULATION/NR_PHY/ulsim.c
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
   int tx_offset;
   double txlev_float;
   int32_t txlev;
-  int start_rb = 30;
+  int start_rb = 0;
   int UE_id =0; // [hna] only works for UE_id = 0 because NUMBER_OF_NR_UE_MAX is set to 1 (phy_init_nr_gNB causes segmentation fault)
 
 
