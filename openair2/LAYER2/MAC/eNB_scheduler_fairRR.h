8708af92ab075047e577dc1735228061b05c1642
laurent <laurent.thomas@open-cells.com>
2019-11-20 17:16:26 +0100

    error in one function prototype

--- openair2/LAYER2/MAC/eNB_scheduler_fairRR.h
+++ openair2/LAYER2/MAC/eNB_scheduler_fairRR.h
@@ -82,7 +82,7 @@ void dlsch_scheduler_pre_ue_select_fairRR(
     frame_t         frameP,
     sub_frame_t     subframeP,
     int*            mbsfn_flag,
-    uint16_t        nb_rbs_required[MAX_NUM_CCs][NUMBER_OF_UE_MAX],
+    uint16_t        nb_rbs_required[MAX_NUM_CCs][MAX_MOBILES_PER_ENB],
     DLSCH_UE_SELECT dlsch_ue_select[MAX_NUM_CCs]);
 
 void dlsch_scheduler_pre_processor_fairRR (module_id_t   Mod_id,
