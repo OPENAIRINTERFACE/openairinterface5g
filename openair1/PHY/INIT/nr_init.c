dd789dc7ba8f742b07c8b3bf06ff18773212efcd
Francesco Mani <francesco.mani@eurecom.fr>
2019-12-05 18:23:53 +0100

    make pbchsim compile again

--- openair1/PHY/INIT/nr_init.c
+++ openair1/PHY/INIT/nr_init.c
@@ -351,6 +351,7 @@ void nr_phy_config_request_sim(PHY_VARS_gNB *gNB,
   gNB->mac_enabled   = 1;
   fp->dl_CarrierFreq = 3500000000;//from_nrarfcn(gNB_config->nfapi_config.rf_bands.rf_band[0],gNB_config->nfapi_config.nrarfcn.value);
   fp->ul_CarrierFreq = 3500000000;//fp->dl_CarrierFreq - (get_uldl_offset(gNB_config->nfapi_config.rf_bands.rf_band[0])*100000);
+  fp->eutra_band = 78;
   fp->threequarter_fs= 0;
   nr_init_frame_parms(gNB_config, fp);
   gNB->configured    = 1;
