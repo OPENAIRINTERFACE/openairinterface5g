1b8e18e05b25ad3ad5851cb7f3c81a428768e6c3
Francesco Mani <francesco.mani@orange.com>
2020-02-03 12:30:45 +0100

    absoluteFrequencySSB is the center of SSB (correction of 10 RB in ref to point A needed)

--- openair2/LAYER2/NR_MAC_UE/config_ue.c
+++ openair2/LAYER2/NR_MAC_UE/config_ue.c
@@ -221,7 +221,7 @@ void config_common_ue(NR_UE_MAC_INST_t *mac,
   if (scc->downlinkConfigCommon->frequencyInfoDL->absoluteFrequencyPointA > 2016666)
     scs_scaling = scs_scaling>>2;
   uint32_t absolute_diff = (*scc->downlinkConfigCommon->frequencyInfoDL->absoluteFrequencySSB - scc->downlinkConfigCommon->frequencyInfoDL->absoluteFrequencyPointA);
-  cfg->ssb_table.ssb_offset_point_a = absolute_diff/(12*scs_scaling);
+  cfg->ssb_table.ssb_offset_point_a = absolute_diff/(12*scs_scaling) - 10;
   cfg->ssb_table.ssb_period = *scc->ssb_periodicityServingCell;
   cfg->ssb_table.ssb_subcarrier_offset = 0; // TODO currently not in RRC?
   switch (scc->ssb_PositionsInBurst->present) {
