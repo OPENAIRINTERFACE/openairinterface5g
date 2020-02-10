064b7f23aac41dfda5c91a00c0c3f570b7adafa8
Raymond Knopp <raymond.knopp@eurecom.fr>
2020-02-10 23:17:27 +0100

    generation of CCEIndex > 0 in gNodeB and modification of unscrambling procedure for PDCCH in UE RX.

--- openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
+++ openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
@@ -1539,7 +1539,7 @@ uint8_t get_num_pdcch_symbols(uint8_t num_dci,DCI_ALLOC_t *dci_alloc,NR_DL_FRAME
 void pdcch_interleaving(NR_DL_FRAME_PARMS *frame_parms,int32_t **z, int32_t **wbar,uint8_t n_symbols_pdcch,uint8_t mi);
 
 void nr_pdcch_unscrambling(uint16_t crnti, NR_DL_FRAME_PARMS *frame_parms, uint8_t slot,
-                           int16_t *z, uint32_t length, uint16_t pdcch_DMRS_scrambling_id);
+                           int16_t *z, int16_t *z2,uint32_t length, uint16_t pdcch_DMRS_scrambling_id);
 
 
 
