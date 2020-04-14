58e973409aaf2b31bbea6fcafabd02099a71a5e0
laurent <laurent.thomas@open-cells.com>
2020-04-14 11:25:59 +0200

    small compilation details

--- openair1/PHY/LTE_TRANSPORT/ulsch_decoding.c
+++ openair1/PHY/LTE_TRANSPORT/ulsch_decoding.c
@@ -314,7 +314,7 @@ int ulsch_decoding_data(PHY_VARS_eNB *eNB, L1_rxtx_proc_t *proc,
     unsigned int Kr_bytes = Kr>>3;
     
 #ifdef DEBUG_ULSCH_DECODING
-    printf("Rate Matching Segment %u (coded bits (G) %d,unpunctured/repeated bits %u, Q_m %d, Nl %d, r_offset %d)...\n",
+    printf("Rate Matching Segment %d (coded bits (G) %d,unpunctured/repeated bits %u, Q_m %d, Nl %d, r_offset %u)...\n",
            r, G,
            Kr*3,
            ulsch_harq->Qm,
@@ -572,7 +572,7 @@ unsigned int  ulsch_decoding(PHY_VARS_eNB *eNB,
   G = G - Q_RI - Q_CQI;
   ulsch_harq->G = G;
   AssertFatal((int)G > 0,
-              "FATAL: ulsch_decoding.c G < 0 (%d) : Q_RI %d, Q_CQI %d\n",G,Q_RI,Q_CQI);
+              "FATAL: ulsch_decoding.c G < 0 (%u) : Q_RI %u, Q_CQI %u\n",G,Q_RI,Q_CQI);
   H = G + Q_CQI;
   Hprime = H/Q_m;
   // Demultiplexing/Deinterleaving of PUSCH/ACK/RI/CQI
--- openair2/RRC/NR/nr_rrc_extern.h
+++ openair2/RRC/NR/nr_rrc_extern.h
@@ -73,7 +73,7 @@ extern UE_PF_PO_t UE_PF_PO[NFAPI_CC_MAX][MAX_MOBILES_PER_ENB];
 
 extern pthread_mutex_t ue_pf_po_mutex;
 
-extern uint16_t reestablish_rnti_map[NUMBER_OF_UE_MAX][2];
+extern uint16_t reestablish_rnti_map[MAX_MOBILES_PER_ENB][2];
 char openair_rrc_gNB_configuration(const module_id_t gnb_mod_idP, gNB_RrcConfigurationReq *configuration);
 
 #endif
