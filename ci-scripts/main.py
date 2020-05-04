29f984a60abb649e56dfe541555ea6ee34272a86
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2020-05-04 16:16:27 +0200

    CI: fix missing class member for EPC
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- ci-scripts/main.py
+++ ci-scripts/main.py
@@ -3583,6 +3583,7 @@ elif re.match('^TesteNB$', mode, re.IGNORECASE) or re.match('^TestUE$', mode, re
 					continue
 				CiTestObj.testCase_id = id
 				HTML.SettestCase_id(CiTestObj.testCase_id)
+				EPC.SetTestCase_id(CiTestObj.testCase_id)
 				CiTestObj.desc = test.findtext('desc')
 				HTML.Setdesc(CiTestObj.desc)
 				action = test.findtext('class')
