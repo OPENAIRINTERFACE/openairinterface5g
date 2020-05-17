6d710966371eb28d1088e1422d2e3554d7a4a0c6
matzakos <panagiotis.matzakos@eurecom.fr>
2020-05-17 13:03:24 +0200

    Change pseudo-subheader byte of random ULSCH PDU (phytest) to an appropriate value
    - Apply a value which cannot be interpreted as any valid LCID at the MAC layer of the receiver gNB (as per 38.321 Table 6.2.1-2),
    to avoid forwarding the random PDU to the upper layers in noS1 mode.

--- openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
+++ openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
@@ -715,10 +715,10 @@ uint32_t nr_ulsch_decoding(PHY_VARS_gNB *phy_vars_gNB,
 
 #ifdef DEBUG_ULSCH_DECODING
   LOG_I(PHY, "Decoder output (payload): \n");
-  for (i = 0; i < harq_process->TBS / 8; i++) {
+  for (i = 0; i < harq_process->TBS ; i++) {
 	  //harq_process_ul_ue->a[i] = (unsigned char) rand();
 	  //printf("a[%d]=0x%02x\n",i,harq_process_ul_ue->a[i]);
-	  printf("0x%02x",harq_process->b[i]);
+	  printf("%02x",harq_process->b[i]);
   }
 #endif
 
