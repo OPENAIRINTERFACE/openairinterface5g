17a1a554b7909c89cb842548339abc55ea198367
Francesco Mani <francesco.mani@eurecom.fr>
2020-06-02 16:14:49 +0200

    separation of dl and ul clear fapi information

--- openair2/LAYER2/NR_MAC_gNB/mac_proto.h
+++ openair2/LAYER2/NR_MAC_gNB/mac_proto.h
@@ -56,10 +56,15 @@ int rrc_mac_config_req_gNB(module_id_t Mod_idP,
 			   NR_CellGroupConfig_t *secondaryCellGroup
                            );
 
-void clear_nr_nfapi_information(gNB_MAC_INST * gNB, 
-                                int CC_idP,
-                                frame_t frameP, 
-                                sub_frame_t subframeP);
+void clear_nr_nfapi_information_dl(gNB_MAC_INST * gNB,
+                                   int CC_idP,
+                                   frame_t frameP,
+                                   sub_frame_t subframeP);
+
+void clear_nr_nfapi_information_ul(gNB_MAC_INST * gNB,
+                                   int CC_idP,
+                                   frame_t frameP,
+                                   sub_frame_t subframeP);
 
 void gNB_dlsch_ulsch_scheduler(module_id_t module_idP,
 			       frame_t frame_txP, sub_frame_t slot_txP,
