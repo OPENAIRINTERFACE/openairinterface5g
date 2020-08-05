a4a7de5ffed08dc2179502c5c7d90f3d8a32d729
Robert Schmidt <robert.schmidt@eurecom.fr>
2020-08-05 08:57:05 +0200

    Make restart work again: do not free/copy ru frame_parms

--- targets/RT/USER/lte-softmodem.c
+++ targets/RT/USER/lte-softmodem.c
@@ -439,8 +439,6 @@ int restart_L1L2(module_id_t enb_id) {
 
   RC.ru_mask |= (1 << ru->idx);
   /* copy the changed frame parameters to the RU */
-  /* TODO this should be done for all RUs associated to this eNB */
-  memcpy(&ru->frame_parms, &RC.eNB[enb_id][0]->frame_parms, sizeof(LTE_DL_FRAME_PARMS));
   set_function_spec_param(RC.ru[enb_id]);
   /* reset the list of connected UEs in the MAC, since in this process with
    * loose all UEs (have to reconnect) */
