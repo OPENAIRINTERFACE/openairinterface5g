260a9e58a05f457e49923ff66aeacaa1460356f9
Raymond Knopp <raymond.knopp@eurecom.fr>
2020-08-27 05:49:46 +0200

    nr_pbchsim modifications for carrier phase rotation procedure

--- openair1/SCHED_NR/phy_procedures_nr_gNB.c
+++ openair1/SCHED_NR/phy_procedures_nr_gNB.c
@@ -293,7 +293,6 @@ void nr_ulsch_procedures(PHY_VARS_gNB *gNB, int frame_rx, int slot_rx, int ULSCH
                           G);
   stop_meas(&gNB->ulsch_decoding_stats);
 
-
   if (ret > gNB->ulsch[ULSCH_id][0]->max_ldpc_iterations){
     LOG_D(PHY, "ULSCH %d in error\n",ULSCH_id);
     nr_fill_indication(gNB,frame_rx, slot_rx, ULSCH_id, harq_pid, 1);
