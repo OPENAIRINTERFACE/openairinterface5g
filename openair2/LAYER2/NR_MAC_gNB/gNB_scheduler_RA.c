c737ea2f8f79637bfe12708d57a8347a5872aa67
Raymond Knopp <raymond.knopp@eurecom.fr>
2020-10-23 16:06:04 +0200

    fixing issue with PRACH0 and multiple PRACH occasions

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -70,7 +70,7 @@ int16_t ssb_index_from_prach(module_id_t module_idP,
   
 	float  num_ssb_per_RO = ssb_per_rach_occasion[cfg->prach_config.ssb_per_rach.value];	
   uint16_t start_symbol_index = 0;
-  uint8_t mu,N_dur,N_t_slot,start_symbol = 0, temp_start_symbol = 0, N_RA_slot;
+  uint8_t mu,N_dur=0,N_t_slot=0,start_symbol = 0, temp_start_symbol = 0, N_RA_slot=0;
   uint16_t format,RA_sfn_index = -1;
 	uint8_t config_period = 1;
   uint16_t prach_occasion_id = -1;
@@ -139,7 +139,7 @@ void find_SSB_and_RO_available(module_id_t module_idP) {
   nfapi_nr_config_request_scf_t *cfg = &RC.nrmac[module_idP]->config[0];
 
   uint8_t config_index = scc->uplinkConfigCommon->initialUplinkBWP->rach_ConfigCommon->choice.setup->rach_ConfigGeneric.prach_ConfigurationIndex;
-  uint8_t mu,N_dur,N_t_slot,start_symbol,N_RA_slot = 0;
+  uint8_t mu,N_dur=0,N_t_slot=0,start_symbol=0,N_RA_slot = 0;
   uint16_t format,N_RA_sfn = 0,unused_RA_occasion,repetition = 0;
 	uint8_t num_active_ssb = 0;
   uint8_t max_association_period = 1;
