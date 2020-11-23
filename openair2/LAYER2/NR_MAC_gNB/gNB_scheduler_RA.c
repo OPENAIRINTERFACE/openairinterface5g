c5b7a3a441a47b112b66586a659ec334b366c3a4
Francesco Mani <francesco.mani@eurecom.fr>
2020-11-23 16:08:01 +0100

    fix scheduling msg2

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -396,12 +396,11 @@ void nr_schedule_msg2(uint16_t rach_frame, uint16_t rach_slot,
 
   // slot and frame limit to transmit msg2 according to response window
   uint8_t slot_limit = (rach_slot + slot_window)%nr_slots_per_frame[mu];
-  //uint8_t frame_limit = (slot_limit>(rach_slot))? rach_frame : (rach_frame +1);
-
+  uint8_t frame_limit = (slot_limit>(rach_slot))? rach_frame : (rach_frame +1);
 
   // go to previous slot if the current scheduled slot is beyond the response window
   // and if the slot is not among the PDCCH monitored ones (38.213 10.1)
-  while ((*msg2_slot>slot_limit) || ((*msg2_frame*nr_slots_per_frame[mu]+*msg2_slot-monitoring_offset)%monitoring_slot_period !=0))  {
+  while (((*msg2_slot>slot_limit)&&(*msg2_frame>frame_limit)) || ((*msg2_frame*nr_slots_per_frame[mu]+*msg2_slot-monitoring_offset)%monitoring_slot_period !=0))  {
     if((*msg2_slot%tdd_period_slot) > 0)
       (*msg2_slot)--;
     else
