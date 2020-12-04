13f0571a6586de3ab8d56299728ba5142fc199db
Robert Schmidt <robert.schmidt@eurecom.fr>
2020-12-04 11:01:46 +0100

    Fix log message: add newline, format

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -989,7 +989,11 @@ void nr_fill_rar(uint8_t Mod_idP,
                  uint8_t * dlsch_buffer,
                  nfapi_nr_pusch_pdu_t  *pusch_pdu){
 
-  LOG_I(MAC, "[gNB] Generate RAR MAC PDU frame %d slot %d preamble index %u", ra->Msg2_frame, ra-> Msg2_slot, ra->preamble_index);
+  LOG_I(MAC,
+        "[gNB] Generate RAR MAC PDU frame %d slot %d preamble index %u\n",
+        ra->Msg2_frame,
+        ra->Msg2_slot,
+        ra->preamble_index);
   NR_RA_HEADER_RAPID *rarh = (NR_RA_HEADER_RAPID *) dlsch_buffer;
   NR_MAC_RAR *rar = (NR_MAC_RAR *) (dlsch_buffer + 1);
   unsigned char csi_req = 0, tpc_command;
