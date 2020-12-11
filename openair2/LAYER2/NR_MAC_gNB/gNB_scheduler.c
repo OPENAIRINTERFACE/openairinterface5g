943d55717e07771463533c9cc6fe96f16ddfdf1f
Robert Schmidt <robert.schmidt@eurecom.fr>
2020-12-11 16:47:10 +0100

    Scheduler: introduce BIT() macro

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -359,8 +359,9 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP,
     nr_rrc_trigger(&ctxt, 0 /*CC_id*/, frame, slot >> *scc->ssbSubcarrierSpacing);
   }
 
-  const uint64_t dlsch_in_slot_bitmap = (1 << 1) | (1 << 3);
-  const uint64_t ulsch_in_slot_bitmap = (1 << 8);
+#define BIT(x) (1 << (x))
+  const uint64_t dlsch_in_slot_bitmap = BIT(1) | BIT(3);
+  const uint64_t ulsch_in_slot_bitmap = BIT(8);
 
   memset(RC.nrmac[module_idP]->cce_list[bwp_id][0],0,MAX_NUM_CCE*sizeof(int)); // coreset0
   memset(RC.nrmac[module_idP]->cce_list[bwp_id][1],0,MAX_NUM_CCE*sizeof(int)); // coresetid 1
