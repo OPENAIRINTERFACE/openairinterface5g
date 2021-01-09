0496c15d273728122e13a8c710e48865ce2b5a2e
Raymond Knopp <raymond.knopp@eurecom.fr>
2021-01-09 11:56:06 +0100

    Remove PHY HARQ round_trials logs

--- openair1/PHY/NR_TRANSPORT/nr_dlsch.c
+++ openair1/PHY/NR_TRANSPORT/nr_dlsch.c
@@ -421,15 +421,8 @@ void dump_pdsch_stats(PHY_VARS_gNB *gNB) {
 
   for (int i=0;i<NUMBER_OF_NR_SCH_STATS_MAX;i++)
     if (gNB->dlsch_stats[i].rnti > 0)
-      LOG_I(PHY,"DLSCH RNTI %x: round_trials %d(%1.1e):%d(%1.1e):%d(%1.1e):%d, current_Qm %d, current_RI %d, total_bytes TX %d\n",
+      LOG_D(PHY,"DLSCH RNTI %x: current_Qm %d, current_RI %d, total_bytes TX %d\n",
 	    gNB->dlsch_stats[i].rnti,
-	    gNB->dlsch_stats[i].round_trials[0],
-	    (double)gNB->dlsch_stats[i].round_trials[1]/gNB->dlsch_stats[i].round_trials[0],
-	    gNB->dlsch_stats[i].round_trials[1],
-	    (double)gNB->dlsch_stats[i].round_trials[2]/gNB->dlsch_stats[i].round_trials[0],
-	    gNB->dlsch_stats[i].round_trials[2],
-	    (double)gNB->dlsch_stats[i].round_trials[3]/gNB->dlsch_stats[i].round_trials[0],
-	    gNB->dlsch_stats[i].round_trials[3],
 	    gNB->dlsch_stats[i].current_Qm,
 	    gNB->dlsch_stats[i].current_RI,
 	    gNB->dlsch_stats[i].total_bytes_tx);
