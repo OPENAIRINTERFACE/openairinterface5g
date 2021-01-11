6c3897d4a79a231721497806d2ce57f5b214bd3d
ChiehChun <chiehchun80348@gmail.com>
2021-01-11 13:50:10 +0100

    Save selected DL HARQ PID in post-processor

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
@@ -580,6 +580,7 @@ void nr_schedule_ue_spec(module_id_t module_id,
                   "no free HARQ process available for UE %d\n",
                   UE_id);
       remove_front_nr_list(&sched_ctrl->available_dl_harq);
+      sched_ctrl->dl_harq_pid = current_harq_pid;
     } else {
       /* PP selected a specific HARQ process. Check whether it will be a new
        * transmission or a retransmission, and remove from the corresponding
