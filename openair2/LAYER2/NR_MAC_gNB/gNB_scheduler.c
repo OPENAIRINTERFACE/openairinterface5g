d973db9273c6bfe3f06af8a3b6bada881febab1d
Sakthivel Velumani <velumani@eurecom.fr>
2021-02-01 13:47:45 +0100

    Fix for dlsch issue on caracal 1. Reducing rx gain at UE. Existing gain seems to saturate the receiver with full bw allocaiton. 2. Setting different dlsch bitmap for phytest mode.

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -360,8 +360,11 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP,
     nr_rrc_trigger(&ctxt, 0 /*CC_id*/, frame, slot >> *scc->ssbSubcarrierSpacing);
   }
 
-  const uint64_t dlsch_in_slot_bitmap = (1 << 1) | (1 << 3);
+  uint64_t dlsch_in_slot_bitmap = (1 << 1) | (1 << 3);
   const uint64_t ulsch_in_slot_bitmap = (1 << 8);
+
+  if (get_softmodem_params()->phy_test) // for testing with OAI-UE
+    dlsch_in_slot_bitmap = (1 << 1);
 
   memset(RC.nrmac[module_idP]->cce_list[bwp_id][0],0,MAX_NUM_CCE*sizeof(int)); // coreset0
   memset(RC.nrmac[module_idP]->cce_list[bwp_id][1],0,MAX_NUM_CCE*sizeof(int)); // coresetid 1
