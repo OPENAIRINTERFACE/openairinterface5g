606fa3c3aa5dca1e31216cbe129aebd3a1d07630
Thomas Schlichter <thomas.schlichter@iis.fraunhofer.de>
2021-03-08 10:59:43 +0100

    fix nr_prachsim: 3.5 GHz is _not_ a valid DL frequency for 30 kHz SCS, so change it to 3.6 GHz

--- openair1/PHY/INIT/nr_init.c
+++ openair1/PHY/INIT/nr_init.c
@@ -431,8 +431,8 @@ void nr_phy_config_request_sim(PHY_VARS_gNB *gNB,
 
   gNB->mac_enabled   = 1;
   if (mu==1) {
-    fp->dl_CarrierFreq = 3500000000;//from_nrarfcn(gNB_config->nfapi_config.rf_bands.rf_band[0],gNB_config->nfapi_config.nrarfcn.value);
-    fp->ul_CarrierFreq = 3500000000;//fp->dl_CarrierFreq - (get_uldl_offset(gNB_config->nfapi_config.rf_bands.rf_band[0])*100000);
+    fp->dl_CarrierFreq = 3600000000;//from_nrarfcn(gNB_config->nfapi_config.rf_bands.rf_band[0],gNB_config->nfapi_config.nrarfcn.value);
+    fp->ul_CarrierFreq = 3600000000;//fp->dl_CarrierFreq - (get_uldl_offset(gNB_config->nfapi_config.rf_bands.rf_band[0])*100000);
     fp->nr_band = 78;
     //  fp->threequarter_fs= 0;
   } else if (mu==3) {
