2c907fb6e5e9030d613e10ddff889ce0469b5f3d
rmagueta <rmagueta@allbesmart.pt>
2021-04-15 15:09:22 +0100

    Fix minor issues of develop

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_bch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_bch.c
@@ -324,6 +324,7 @@ void schedule_control_sib1(module_id_t module_id,
     gNB_mac->sched_ctrlCommon = calloc(1,sizeof(*gNB_mac->sched_ctrlCommon));
     gNB_mac->sched_ctrlCommon->search_space = calloc(1,sizeof(*gNB_mac->sched_ctrlCommon->search_space));
     gNB_mac->sched_ctrlCommon->coreset = calloc(1,sizeof(*gNB_mac->sched_ctrlCommon->coreset));
+    gNB_mac->sched_ctrlCommon->active_bwp = calloc(1,sizeof(*gNB_mac->sched_ctrlCommon->active_bwp));
     fill_default_searchSpaceZero(gNB_mac->sched_ctrlCommon->search_space);
     fill_default_coresetZero(gNB_mac->sched_ctrlCommon->coreset,servingcellconfigcommon);
     fill_default_initialDownlinkBWP(gNB_mac->sched_ctrlCommon->active_bwp,servingcellconfigcommon);
