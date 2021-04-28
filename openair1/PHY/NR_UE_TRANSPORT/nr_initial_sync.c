cc15ac25b9a8a278537cd239e325d6ac9fb094ed
francescomani <francesco.mani@eurecom.fr>
2021-04-28 17:49:27 +0200

    simplification of previous fix

--- openair1/PHY/NR_UE_TRANSPORT/nr_initial_sync.c
+++ openair1/PHY/NR_UE_TRANSPORT/nr_initial_sync.c
@@ -309,8 +309,8 @@ int nr_initial_sync(UE_nr_rxtx_proc_t *proc, PHY_VARS_NR_UE *ue, int n_frames)
         int n_symb_prefix0 = (ue->symbol_offset/(7*(1<<mu)))+1;
         sync_pos_frame = n_symb_prefix0*(fp->ofdm_symbol_size + fp->nb_prefix_samples0)+(ue->symbol_offset-n_symb_prefix0)*(fp->ofdm_symbol_size + fp->nb_prefix_samples);
         if (ue->ssb_offset < sync_pos_frame){
-          ue->rx_offset = (2*fp->samples_per_frame) - sync_pos_frame + ue->ssb_offset;
-          ue->init_sync_frame = !is + 2;
+          ue->rx_offset = fp->samples_per_frame - sync_pos_frame + ue->ssb_offset;
+          ue->init_sync_frame = !is + 1;
         }
         else{
           ue->rx_offset = ue->ssb_offset - sync_pos_frame;
