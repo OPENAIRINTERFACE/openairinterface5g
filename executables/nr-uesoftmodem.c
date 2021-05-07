31d73a7bf2d861e26e7efba5816bc1060e1a1dc4
matzakos <panagiotis.matzakos@eurecom.fr>
2021-05-07 15:06:52 +0200

    Remove artificial triggering of RRC Setup Request from nrUE

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -530,14 +530,6 @@ int main( int argc, char **argv ) {
   // wait for end of program
   printf("TYPE <CTRL-C> TO TERMINATE\n");
 
-  //Don't understand why generation of RRCSetupRequest is called here. It seems wrong
-  protocol_ctxt_t ctxt_pP = {0};
-  ctxt_pP.enb_flag = ENB_FLAG_NO;
-  ctxt_pP.rnti = 0x1234;
-  RC.nrrrc = (gNB_RRC_INST **)malloc(1*sizeof(gNB_RRC_INST *));
-  RC.nrrrc[0] = (gNB_RRC_INST*)malloc(sizeof(gNB_RRC_INST));
-  RC.nrrrc[0]->node_type = ngran_gNB;
-  nr_rrc_ue_generate_RRCSetupRequest(ctxt_pP.module_id, 0);
   if (create_tasks_nrue(1) < 0) {
     printf("cannot create ITTI tasks\n");
     exit(-1); // need a softer mode
