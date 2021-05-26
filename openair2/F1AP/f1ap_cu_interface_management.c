65d24ae205dd5ba296345c9760c3273ab3829843
rmagueta <rmagueta@allbesmart.pt>
2021-05-26 11:37:21 +0100

    Add condition in CU_handle_F1_SETUP_REQUEST to LTE version

--- openair2/F1AP/f1ap_cu_interface_management.c
+++ openair2/F1AP/f1ap_cu_interface_management.c
@@ -103,7 +103,11 @@ int CU_handle_F1_SETUP_REQUEST(instance_t instance,
               assoc_id, stream);
   }
 
-  message_p = itti_alloc_new_message(TASK_CU_F1, 0, F1AP_SETUP_REQ);
+  if (RC.nrrrc && RC.nrrrc[0]->node_type == ngran_gNB_CU) {
+    message_p = itti_alloc_new_message(TASK_CU_F1, 0, F1AP_SETUP_REQ);
+  } else {
+    message_p = itti_alloc_new_message(TASK_RRC_ENB, 0, F1AP_SETUP_REQ);
+  }
 
   /* assoc_id */
   F1AP_SETUP_REQ(message_p).assoc_id = assoc_id;
