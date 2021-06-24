c249ca0bd826204d4d82b532a2a5fc3cc29294fb
francescomani <francesco.mani@eurecom.fr>
2021-06-24 12:22:03 +0200

    fix for empty mib at beginning of execution

--- openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
+++ openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
@@ -736,6 +736,7 @@ typedef struct gNB_MAC_INST_s {
   NR_CellGroupConfig_t *secondaryCellGroupCommon;
   NR_Type0_PDCCH_CSS_config_t type0_PDCCH_CSS_config[64];
 
+  bool first_MIB;
 } gNB_MAC_INST;
 
 #endif /*__LAYER2_NR_MAC_GNB_H__ */
