3c9ca36651e45df9635a41a0276d815e1a8eff1b
rmagueta <rmagueta@allbesmart.pt>
2021-07-09 13:22:30 +0100

    Fix fill slot bitmap for FR2

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -340,7 +340,7 @@ void schedule_nr_SRS(module_id_t module_idP, frame_t frameP, sub_frame_t subfram
 
 
 bool is_xlsch_in_slot(uint64_t bitmap, sub_frame_t slot) {
-  if (slot>64) return false; //quickfix for FR2 where there are more than 64 slots (bitmap to be removed)
+  if (slot>=64) return false; //quickfix for FR2 where there are more than 64 slots (bitmap to be removed)
   return (bitmap >> slot) & 0x01;
 }
 
