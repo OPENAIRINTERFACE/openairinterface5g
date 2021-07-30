8bb80c9ef0ef5c0f8b1538ce0b590c58dcca66c0
francescomani <francesco.mani@eurecom.fr>
2021-07-30 12:18:14 +0200

    another attempt to fix ping and iper issues by calling commented functions

--- openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
+++ openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
@@ -60,13 +60,8 @@ NR_UE_MAC_INST_t * nr_l2_init_ue(NR_UE_RRC_INST_t* rrc_inst) {
 
       nr_rrc_mac_config_req_ue(0,0,0,NULL,NULL,NULL,rrc_inst->scell_group_config);
       AssertFatal(rlc_module_init(0) == 0, "%s: Could not initialize RLC layer\n", __FUNCTION__);
-      
-      // if (IS_SOFTMODEM_NOS1){
-      //if (1) {
-      //  AssertFatal(rlc_module_init(0) == 0, "%s: Could not initialize RLC layer\n", __FUNCTION__);
-      //  nr_pdcp_layer_init();
-        //nr_DRB_preconfiguration(nr_ue_mac_inst->crnti);
-      //}
+      pdcp_layer_init();
+      nr_DRB_preconfiguration(nr_ue_mac_inst->crnti);
 
       // Allocate memory for ul_config_request in the mac instance. This is now a pointer and will
       // point to a list of structures (one for each UL slot) to store PUSCH scheduling parameters
