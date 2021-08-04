8cc1d0a2dea83a3b7c2af09c112fe5babf2bf55a
francescomani <francesco.mani@eurecom.fr>
2021-08-04 11:21:07 +0200

    initialization of pdcp only in nos1

--- openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
+++ openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
@@ -60,9 +60,10 @@ NR_UE_MAC_INST_t * nr_l2_init_ue(NR_UE_RRC_INST_t* rrc_inst) {
 
       nr_rrc_mac_config_req_ue(0,0,0,NULL,NULL,NULL,rrc_inst->scell_group_config);
       AssertFatal(rlc_module_init(0) == 0, "%s: Could not initialize RLC layer\n", __FUNCTION__);
-      pdcp_layer_init();
-      nr_DRB_preconfiguration(nr_ue_mac_inst->crnti);
-
+      if (IS_SOFTMODEM_NOS1){
+        pdcp_layer_init();
+        nr_DRB_preconfiguration(nr_ue_mac_inst->crnti);
+      }
       // Allocate memory for ul_config_request in the mac instance. This is now a pointer and will
       // point to a list of structures (one for each UL slot) to store PUSCH scheduling parameters
       // received from UL DCI.
