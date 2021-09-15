18d66873f3b4afe65b1139449418c214a5aa2a8d
hardy <remi.hardy@openairinterface.org>
2021-09-15 09:54:29 +0200

    fixing dnn to use with OAICN5G in infra yaml

--- ci-scripts/epc.py
+++ ci-scripts/epc.py
@@ -466,7 +466,7 @@ class EPCManagement():
 			self.LogCollectOAICN5G()
 			logging.debug('Terminating OAI CN5G')
 			mySSH.command('cd /opt/oai-cn5g-fed/docker-compose', '\$', 5)
-			mySSH.command('./core-network.sh stop nrf spgwu', '\$', 30)
+			mySSH.command('./core-network.sh stop nrf spgwu', '\$', 60)
 		else:
 			logging.error('This should not happen!')
 		mySSH.close()
