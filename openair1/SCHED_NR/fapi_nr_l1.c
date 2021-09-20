bfd9671626b11150a880e4855d5ebe52d9b6516e
francescomani <francesco.mani@eurecom.fr>
2021-09-20 19:32:19 +0200

    fixes in acknack scheduling

--- openair1/SCHED_NR/fapi_nr_l1.c
+++ openair1/SCHED_NR/fapi_nr_l1.c
@@ -104,8 +104,7 @@ void handle_nfapi_nr_pdcch_pdu(PHY_VARS_gNB *gNB,
 			       int frame, int slot,
 			       nfapi_nr_dl_tti_pdcch_pdu *pdcch_pdu) {
 
-  if(pdcch_pdu->pdcch_pdu_rel15.CoreSetType==1)
-    LOG_I(PHY,"Frame %d, Slot %d: DCI processing - proc:slot_tx:%d pdcch_pdu_rel15->numDlDci:%d\n",frame,slot, slot, pdcch_pdu->pdcch_pdu_rel15.numDlDci);
+  LOG_D(PHY,"Frame %d, Slot %d: DCI processing - proc:slot_tx:%d pdcch_pdu_rel15->numDlDci:%d\n",frame,slot, slot, pdcch_pdu->pdcch_pdu_rel15.numDlDci);
 
   // copy dci configuration into gNB structure
   //  gNB->pdcch_pdu = pdcch_pdu;
