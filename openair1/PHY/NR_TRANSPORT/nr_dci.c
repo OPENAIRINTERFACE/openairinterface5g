df4a57096bcbccde41fd276e29577c8a3f803d1f
francescomani <francesco.mani@eurecom.fr>
2021-09-28 11:28:14 +0200

    dlsim fix

--- openair1/PHY/NR_TRANSPORT/nr_dci.c
+++ openair1/PHY/NR_TRANSPORT/nr_dci.c
@@ -133,7 +133,7 @@ void nr_generate_dci(PHY_VARS_gNB *gNB,
 						   0,NULL);
     polar_encoder_fast((uint64_t*)dci_pdu->Payload, (void*)encoder_output, n_RNTI,1,currentPtr);
 #ifdef DEBUG_CHANNEL_CODING
-    printf("polar rnti %x,length %d, L %d\n",n_RNTI, dci_pdu->PayloadSizeBits,pdcch_pdu_rel15->dci_pdu.AggregationLevel[d]);
+    printf("polar rnti %x,length %d, L %d\n",n_RNTI, dci_pdu->PayloadSizeBits,pdcch_pdu_rel15->dci_pdu->AggregationLevel);
     printf("DCI PDU: [0]->0x%lx \t [1]->0x%lx\n",
 	   ((uint64_t*)dci_pdu->Payload)[0], ((uint64_t*)dci_pdu->Payload)[1]);
     printf("Encoded Payload (length:%d dwords):\n", encoded_length>>5);
