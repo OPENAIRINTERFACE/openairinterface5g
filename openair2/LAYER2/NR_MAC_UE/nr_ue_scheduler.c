41e2b21d08504b80ba3b18647f091f3b53de297a
mjoang <m.joa-ng@northeastern.edu>
2021-11-15 13:54:25 -0500

    Change missing mutex init per review.

--- openair2/LAYER2/NR_MAC_UE/nr_ue_scheduler.c
+++ openair2/LAYER2/NR_MAC_UE/nr_ue_scheduler.c
@@ -948,7 +948,6 @@ NR_UE_L2_STATE_t nr_ue_scheduler(nr_downlink_indication_t *dl_info, nr_uplink_in
     if (ul_config){
       pthread_mutex_lock(&ul_config->mutex_ul_config);
       if ((ul_info->slot_tx == ul_config->slot && ul_info->frame_tx == ul_config->sfn) && ul_config->number_pdus > 0){
-        pthread_mutex_unlock(&ul_config->mutex_ul_config);
 
         LOG_D(NR_MAC, "In %s:[%d.%d]: number of UL PDUs: %d with UL transmission in [%d.%d]\n", __FUNCTION__, frame_tx, slot_tx, ul_config->number_pdus, ul_config->sfn, ul_config->slot);
 
@@ -959,7 +958,6 @@ NR_UE_L2_STATE_t nr_ue_scheduler(nr_downlink_indication_t *dl_info, nr_uplink_in
         tx_req.sfn = frame_tx;
         tx_req.number_of_pdus = 0;
 
-        pthread_mutex_lock(&ul_config->mutex_ul_config);
         for (int j = 0; j < ul_config->number_pdus; j++) {
           uint8_t *ulsch_input_buffer = &(ulsch_input_buffer_array[tx_req.number_of_pdus][MAX_ULSCH_PAYLOAD_BYTES]);
 
@@ -1020,16 +1018,15 @@ NR_UE_L2_STATE_t nr_ue_scheduler(nr_downlink_indication_t *dl_info, nr_uplink_in
             }
           }
         }
-        pthread_mutex_unlock(&ul_config->mutex_ul_config);
+        pthread_mutex_unlock(&ul_config->mutex_ul_config); // avoid double lock
 
         fill_scheduled_response(&scheduled_response, NULL, ul_config, &tx_req, mod_id, cc_id, rx_frame, rx_slot, ul_info->thread_id);
         if(mac->if_module != NULL && mac->if_module->scheduled_response != NULL){
           mac->if_module->scheduled_response(&scheduled_response);
         }
+        pthread_mutex_lock(&ul_config->mutex_ul_config);
       }
-      else {
-        pthread_mutex_unlock(&ul_config->mutex_ul_config);
-      }
+      pthread_mutex_unlock(&ul_config->mutex_ul_config);
     }
   }
 
