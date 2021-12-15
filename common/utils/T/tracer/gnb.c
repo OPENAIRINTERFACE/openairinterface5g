ecd09b3068cd230ef7c1de5288a541b7b4fcbeec
rmagueta <rmagueta@allbesmart.pt>
2021-12-15 14:39:31 +0000

    Change variable name

--- common/utils/T/tracer/gnb.c
+++ common/utils/T/tracer/gnb.c
@@ -17,13 +17,13 @@ int next_ue_id;
 
 typedef struct {
   widget *pucch_pusch_iq_plot;
-  widget *ul_estimate_ue_xy_plot;
+  widget *ul_freq_estimate_ue_xy_plot;
   widget *current_ue_label;
   widget *current_ue_button;
   widget *prev_ue_button;
   widget *next_ue_button;
   logger *pucch_pusch_iq_logger;
-  logger *ul_estimate_ue_logger;
+  logger *ul_freq_estimate_ue_logger;
 } gnb_gui;
 
 typedef struct {
@@ -92,7 +92,7 @@ static void set_current_ue(gui *g, gnb_data *e, int ue)
   xy_plot_set_title(g, e->e->pucch_pusch_iq_plot, s);
 
   sprintf(s, "UL channel estimation in frequency domain [UE %d]", ue);
-  xy_plot_set_title(g, e->e->ul_estimate_ue_xy_plot, s);
+  xy_plot_set_title(g, e->e->ul_freq_estimate_ue_xy_plot, s);
 }
 
 void reset_ue_ids(void)
@@ -181,14 +181,14 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
 
   /* UL estimated channel */
   w = new_xy_plot(g, 600, 200, "", 50);
-  e->ul_estimate_ue_xy_plot = w;
+  e->ul_freq_estimate_ue_xy_plot = w;
   widget_add_child(g, line, w, -1);
   xy_plot_set_range(g, w, 0, 100*10, -10, 80);
   l = new_framelog(h, database, "GNB_PHY_UL_CHANNEL_ESTIMATE", "subframe", "chest_t");
   framelog_set_update_only_at_sf9(l, 0);
   v = new_view_xy(100*10, 10, g, w, new_color(g, "#0c0c72"), XY_LOOP_MODE);
   logger_add_view(l, v);
-  e->ul_estimate_ue_logger = l;
+  e->ul_freq_estimate_ue_logger = l;
 
   set_current_ue(g, ed, ed->ue);
   register_notifier(g, "click", e->current_ue_button, click, ed);
