5ead416cd042a66c47feed35159fea8937d1208f
Laurent THOMAS <laurent.thomas@open-cells.com>
2021-12-28 16:58:23 +0100

    remove volonatry process maximum life time of 600sec (episys?)

--- common/utils/utils.c
+++ common/utils/utils.c
@@ -143,21 +143,6 @@ void *memcpy1(void *dst,const void *src,size_t n) {
 
 void set_priority(int priority)
 {
-  /* When running with a realtime scheduler, a buggy run-away process can take
-     down the host requiring a reboot to recover.  To avoid this scenario, we
-     use alarm(2) to set a maximum running time */
-  unsigned max_runtime_seconds = 10 * 60; /* default */
-  const char *env = getenv("MAX_RUNTIME_SECONDS");
-  if (env != NULL)
-  {
-    max_runtime_seconds = atoi(env);
-  }
-  unsigned was_alarm = alarm(max_runtime_seconds);
-  /* Normally, was_alarm should be 0 indicating there was no previous alarm set.
-     A non-zero value could indicate a mistake */
-  fprintf(stderr, "Set alarm for %u seconds, was %u (see $MAX_RUNTIME_SECONDS)\n",
-          max_runtime_seconds, was_alarm);
-
   struct sched_param param =
   {
     .sched_priority = priority,
