6146f44b808d9a671186e37b6d44100e79f9cba1
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-03-01 18:50:53 +0100

    CI: log to see why RT stats checking fails

--- ci-scripts/ran.py
+++ ci-scripts/ran.py
@@ -1137,6 +1137,7 @@ class RANManagement():
 				#check if there is a fail => will render the test as failed
 				for k in datalog_rt_stats['Data']:
 					if float(datalog_rt_stats['Data'][k][2])> datalog_rt_stats['Threshold'][k]: #condition for fail : avg/ref is greater than the fixed threshold
+						logging.debug('\u001B[1;30;43m datalog_rt_stats metric ' + k + '=' + datalog_rt_stats['Data'][k][2] + ' > threshold ' + str(datalog_rt_stats['Threshold'][k]) + ' \u001B[0m')
 						RealTimeProcessingIssue = True
 			else:
 				statMsg = 'No real time stats found in the log file\n'
