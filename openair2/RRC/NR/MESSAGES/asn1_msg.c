85e879b1ea47304e42a0b6c5d1497b7d814451b5
francescomani <email@francescomani.it>
2022-03-18 10:30:12 +0100

    remove call to csirs configuration in SA (not working yet, asn1 encoding fails)

--- openair2/RRC/NR/MESSAGES/asn1_msg.c
+++ openair2/RRC/NR/MESSAGES/asn1_msg.c
@@ -1462,17 +1462,6 @@ void update_cellGroupConfig(NR_CellGroupConfig_t *cellGroupConfig,
 
   NR_ServingCellConfigCommon_t *scc = carrier->servingcellconfigcommon;
 
-  NR_CSI_MeasConfig_t *csi_MeasConfig;
-  if(!SpCellConfig->spCellConfigDedicated->csi_MeasConfig) {
-    csi_MeasConfig = calloc(1,sizeof(*csi_MeasConfig));
-    SpCellConfig->spCellConfigDedicated->csi_MeasConfig->choice.setup = csi_MeasConfig;
-  }
-  else
-    csi_MeasConfig = SpCellConfig->spCellConfigDedicated->csi_MeasConfig->choice.setup;
-
-  int curr_bwp = NRRIV2BW(scc->downlinkConfigCommon->initialDownlinkBWP->genericParameters.locationAndBandwidth,MAX_BWP_SIZE);
-  config_csirs(scc, csi_MeasConfig, uid, carrier->pdsch_AntennaPorts, curr_bwp, carrier->do_CSIRS);
-
   NR_BWP_DownlinkDedicated_t *bwp_Dedicated = SpCellConfig->spCellConfigDedicated->initialDownlinkBWP;
   set_dl_mcs_table(scc->downlinkConfigCommon->initialDownlinkBWP->genericParameters.subcarrierSpacing,
                    uecap, bwp_Dedicated, scc);
