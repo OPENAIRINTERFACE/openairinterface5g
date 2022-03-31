e0befd2a8f3ffd96e0ad09e23df771f32620e3ef
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2022-03-31 10:15:47 +0200

    fix(ci): 2 little typos   - The GitLab pipeline reporting for the 5G RF sim container was the same as 4G RF   - Calling a sudo command without providing password
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- ci-scripts/cls_module_ue.py
+++ ci-scripts/cls_module_ue.py
@@ -76,7 +76,7 @@ class Module_UE:
 			logging.debug('Starting ' + self.Process['Name'])
 			mySSH = sshconnection.SSHConnection()
 			mySSH.open(self.HostIPAddress, self.HostUsername, self.HostPassword)
-			mySSH.command('sudo rm -f /tmp/quectel-cm.log','\$',5)
+			mySSH.command('echo ' + self.HostPassword + ' | sudo -S rm -f /tmp/quectel-cm.log','\$',5)
 			mySSH.command('echo $USER; echo ' + self.HostPassword + ' | nohup sudo -S stdbuf -o0 ' + self.Process['Cmd'] + ' ' +  self.Process['Apn'][CNType]  + ' > /tmp/quectel-cm.log 2>&1 &','\$',5)
 			mySSH.close()
 			#checking the process
