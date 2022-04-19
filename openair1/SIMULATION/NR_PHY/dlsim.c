795c057d8cddcbfb0e50a179d57a18ed8429d08b
Thomas Schlichter <thomas.schlichter@iis.fraunhofer.de>
2022-04-19 11:32:47 +0200

    nr_dlsim: fix low-SNR performance by using DCI aggregation level 8

--- openair1/SIMULATION/NR_PHY/dlsim.c
+++ openair1/SIMULATION/NR_PHY/dlsim.c
@@ -275,10 +275,17 @@ void nr_dlsim_preprocessor(module_id_t module_id,
   /* manually set free CCE to 0 */
   const int target_ss = NR_SearchSpace__searchSpaceType_PR_ue_Specific;
   sched_ctrl->search_space = get_searchspace(NULL, scc, sched_ctrl->active_bwp ? sched_ctrl->active_bwp->bwp_Dedicated : NULL, target_ss);
-  uint8_t nr_of_candidates;
-  find_aggregation_candidates(&sched_ctrl->aggregation_level,
-                              &nr_of_candidates,
-                              sched_ctrl->search_space,4);
+  uint8_t nr_of_candidates = 0;
+  if (g_mcsIndex < 4) {
+    find_aggregation_candidates(&sched_ctrl->aggregation_level,
+                                &nr_of_candidates,
+                                sched_ctrl->search_space,8);
+  }
+  if (nr_of_candidates == 0) {
+    find_aggregation_candidates(&sched_ctrl->aggregation_level,
+                                &nr_of_candidates,
+                                sched_ctrl->search_space,4);
+  }
   sched_ctrl->coreset = get_coreset(module_id, scc, sched_ctrl->active_bwp->bwp_Dedicated, sched_ctrl->search_space, target_ss);
   sched_ctrl->cce_index = 0;
 
