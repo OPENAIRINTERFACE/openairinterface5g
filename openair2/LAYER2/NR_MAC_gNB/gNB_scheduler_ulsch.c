1060de4c323aa774060f62ae504925ea0f09be10
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-04-20 12:29:16 +0200

    Correct frame calculation in UL scheduler

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
@@ -1479,7 +1479,7 @@ bool nr_fr1_ulsch_preprocessor(module_id_t module_id, frame_t frame, sub_frame_t
   if (tda < 0)
     return false;
   int K2 = get_K2(scc, scc_sib1, sched_ctrl->active_ubwp, tda, mu);
-  const int sched_frame = frame + (slot + K2 >= nr_slots_per_frame[mu]);
+  const int sched_frame = (frame + (slot + K2 >= nr_slots_per_frame[mu])) % 1024;
   const int sched_slot = (slot + K2) % nr_slots_per_frame[mu];
 
   if (!is_xlsch_in_slot(nr_mac->ulsch_slot_bitmap[sched_slot / 64], sched_slot))
