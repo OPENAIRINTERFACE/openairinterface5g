b112ed2c6be86a4d420afb3494af97a4ffc03649
El Mghazli Yacine <yacine.el_mghazli@nokia-bell-labs.com>
2022-04-22 17:30:41 +0200

    dynamic tables

--- common/utils/websrv/frontend/src/app/components/commands/commands.component.ts
+++ common/utils/websrv/frontend/src/app/components/commands/commands.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/internal/operators/map';
-import { tap, mergeMap } from 'rxjs/operators';
-import { CommandsApi, IArgType, ILog } from 'src/app/api/commands.api';
+import { CommandsApi, IArgType, IColumn, IRow } from 'src/app/api/commands.api';
 import { CmdCtrl } from 'src/app/controls/cmd.control';
+import { RowCtrl } from 'src/app/controls/row.control';
 import { VarCtrl } from 'src/app/controls/var.control';
 import { DialogService } from 'src/app/services/dialog.service';
 import { LoadingService } from 'src/app/services/loading.service';
@@ -32,14 +32,9 @@ export class CommandsComponent {
   // selectedArg?: VarCtrl
 
   //table columns
-  DISPLAYED_COLUMNS = [
-    'component',
-    'level',
-    'output',
-    'enabled'
-  ]
-
-  logs$: Observable<ILog[]> = new Observable<ILog[]>()
+  displayedColumns: string[] = []
+  rows$: Observable<RowCtrl[]> = new Observable<RowCtrl[]>()
+  columns: IColumn[] = []
 
   constructor(
     public commandsApi: CommandsApi,
@@ -92,9 +87,14 @@ export class CommandsComponent {
   }
 
   onCmdSubmit(control: CmdCtrl) {
-    this.logs$ = this.commandsApi.runCommand$(control.api(), `${this.selectedModule!.nameFC.value}`).pipe(
-      tap(resp => this.success('runCommand ' + control.nameFC.value + ' OK', resp.display!.join("</p><p>"))),
-      map(iresp => iresp.logs!)
+    this.rows$ = this.commandsApi.runCommand$(control.api(), `${this.selectedModule!.nameFC.value}`).pipe(
+      map(iresp => {
+        this.columns = iresp.columns
+        this.displayedColumns = this.columns.map(col => col.name)
+        let rows: RowCtrl[] = []
+        iresp.rows.map(row => this.columns.map(col => rows.push(new RowCtrl(row, col.type))))
+        return rows
+      })
     );
   }
 
---
+++ common/utils/websrv/frontend/src/app/controls/row.control.ts
@@ -0,0 +1,72 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { IArgType, IRow } from '../api/commands.api';
+
+
+
+export class RowCtrl extends FormGroup {
+
+  type: IArgType
+
+  constructor(irow: IRow, type: IArgType) {
+    super({});
+    this.type = type
+    this.addControl(type, new FormControl(irow));
+  }
+
+  api() {
+
+    switch (this.type) {
+      case IArgType.boolean:
+        return this.boolFC.value ? "true" : "false";
+      case IArgType.list:
+        return this.listFC.value ? "true" : "false";
+      case IArgType.number:
+        return this.numberFC.value ? "true" : "false";
+      case IArgType.range:
+        return this.rangeFC.value ? "true" : "false";
+      case IArgType.string:
+        return this.stringFC.value
+    }
+  }
+
+  get boolFC() {
+    return this.get(IArgType.boolean) as FormControl;
+  }
+
+  set boolFC(control: FormControl) {
+    this.setControl(IArgType.boolean, control);
+  }
+
+  get listFC() {
+    return this.get(IArgType.list) as FormControl;
+  }
+
+  set listFC(control: FormControl) {
+    this.setControl(IArgType.list, control);
+  }
+
+  get numberFC() {
+    return this.get(IArgType.number) as FormControl;
+  }
+
+  set numberFC(control: FormControl) {
+    this.setControl(IArgType.number, control);
+  }
+
+  get rangeFC() {
+    return this.get(IArgType.range) as FormControl;
+  }
+
+  set rangeFC(control: FormControl) {
+    this.setControl(IArgType.range, control);
+  }
+
+  get stringFC() {
+    return this.get(IArgType.string) as FormControl;
+  }
+
+  set stringFC(control: FormControl) {
+    this.setControl(IArgType.string, control);
+  }
+}
+
