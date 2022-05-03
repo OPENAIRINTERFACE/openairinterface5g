bf5389936def5746f22abdd577525517d41c866d
Raymond Knopp <Raymond Knopp>
2022-05-03 22:09:46 +0200

    added deletion/creation of ldpc_gen_files directory and subdirectories

--- openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/run_ldpc_generators.sh
+++ openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/run_ldpc_generators.sh
@@ -1,3 +1,11 @@
+rm -rf $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/cnProc
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/cnProc_avx512
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/bnProc
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/bnProc_avx512
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/bnProcPc
+mkdir $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/ldpc_gen_files/bnProcPc_avx512
 cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_bnProc; make;./bnProc_gen_avx2
 cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_cnProc; make;./cnProc_gen_avx2
 cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_bnProc_avx512;make;./bnProc_gen_avx512
