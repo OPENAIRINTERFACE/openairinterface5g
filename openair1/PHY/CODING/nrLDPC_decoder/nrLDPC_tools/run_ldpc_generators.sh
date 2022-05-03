3ecfead572a6718363fab05e4d30143a85ebcc68
Raymond Knopp <Raymond Knopp>
2022-05-03 22:03:19 +0200

    added script to generate ldpc decoder files

---
+++ openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/run_ldpc_generators.sh
@@ -0,0 +1,4 @@
+cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_bnProc; make;./bnProc_gen_avx2
+cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_cnProc; make;./cnProc_gen_avx2
+cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_bnProc_avx512;make;./bnProc_gen_avx512
+cd $OPENAIR_HOME/openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_tools/generator_cnProc_avx512;make;./cnProc_gen_avx512
