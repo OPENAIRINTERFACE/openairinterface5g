3b165473741a73420920446a33b41cf38c03e592
francescomani <email@francescomani.it>
2022-05-10 09:13:52 +0200

    fix in pbchsim

--- openair1/SIMULATION/NR_PHY/pbchsim.c
+++ openair1/SIMULATION/NR_PHY/pbchsim.c
@@ -457,7 +457,7 @@ int main(int argc, char **argv)
 
   nr_phy_config_request_sim_pbchsim(gNB,N_RB_DL,N_RB_DL,mu,Nid_cell,SSB_positions);
   phy_init_nr_gNB(gNB,0,1);
-  fp->ssb_start_subcarrier = 12 * gNB->gNB_config->ssb_table.ssb_offset_point_a.value
+  frame_parms->ssb_start_subcarrier = 12 * gNB->gNB_config.ssb_table.ssb_offset_point_a.value;
 
   uint8_t n_hf = 0;
   int cyclic_prefix_type = NFAPI_CP_NORMAL;
