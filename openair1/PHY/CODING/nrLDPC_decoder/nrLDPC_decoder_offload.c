e8fc4da2b69e5d127e6efc9550df788255a91bef
Hongzhi Wang <hongzhi.wang@openairinterface.org>
2022-06-22 17:26:30 +0200

    update offload decoder license and notes

--- openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_decoder_offload.c
+++ openair1/PHY/CODING/nrLDPC_decoder/nrLDPC_decoder_offload.c
@@ -1,24 +1,3 @@
-/*
- * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under
- * the OAI Public License, Version 1.1  (the "License"); you may not use this file
- * except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.openairinterface.org/?page_id=698
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *-------------------------------------------------------------------------------
- * For more information about the OpenAirInterface (OAI) Software Alliance:
- *      contact@openairinterface.org
- */
-
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2017 Intel Corporation
  */
@@ -28,7 +7,7 @@
  * \author openairinterface 
  * \date 12-06-2021
  * \version 1.0
- * \note: based on testbbdev functions. Harq buffer offset added.
+ * \note: based on testbbdev test_bbdev_perf.c functions. Harq buffer offset added.
  * \mbuf and mempool allocated at the init step, LDPC parameters updated from OAI.
  * \warning
  */
