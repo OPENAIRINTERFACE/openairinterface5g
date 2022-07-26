3d93d0dfc092404004f57ad57a137cce3c363eb8
francescomani <email@francescomani.it>
2022-07-26 18:41:19 +0200

    fix for pusch dtx in phytest mode

--- openair1/PHY/NR_TRANSPORT/nr_transport_proto.h
+++ openair1/PHY/NR_TRANSPORT/nr_transport_proto.h
@@ -121,11 +121,11 @@ void free_gNB_dlsch(NR_gNB_DLSCH_t **dlschptr, uint16_t N_RB, const NR_DL_FRAME_
     @param slot Slot number
     @param harq_pid HARQ process ID
 */
-int nr_rx_pusch(PHY_VARS_gNB *gNB,
-                uint8_t UE_id,
-                uint32_t frame,
-                uint8_t slot,
-                unsigned char harq_pid);
+void nr_rx_pusch(PHY_VARS_gNB *gNB,
+                 uint8_t UE_id,
+                 uint32_t frame,
+                 uint8_t slot,
+                 unsigned char harq_pid);
 
 /** \brief This function performs RB extraction (signal and channel estimates) (currently signal only until channel estimation and compensation are implemented)
     @param rxdataF pointer to the received frequency domain signal
