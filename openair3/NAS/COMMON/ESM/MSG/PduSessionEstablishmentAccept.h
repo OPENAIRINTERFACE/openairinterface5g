e057c85e60b49de47d4b598c03cf8d5f34be0176
Angelo Athanassopoulos <angelo.athanassopoulos@hhi.fraunhofer.de>
2022-10-05 13:33:25 +0200

    PSEA - Macros for Optional IE Identifiers

--- openair3/NAS/COMMON/ESM/MSG/PduSessionEstablishmentAccept.h
+++ openair3/NAS/COMMON/ESM/MSG/PduSessionEstablishmentAccept.h
@@ -24,4 +24,17 @@
 
 #include <stdint.h>
 
+/* PDU Session Establish Accept Optional IE Identifiers - TS 24.501 Table 8.3.2.1.1 */
+
+#define IEI_5GSM_CAUSE      0x59 /* 5GSM cause 9.11.4.2  */
+#define IEI_PDU_ADDRESS     0x29 /* PDU address 9.11.4.10 */
+#define IEI_RQ_TIMER_VALUE  0x56 /* GPRS timer 9.11.2.3  */
+#define IEI_SNSSAI          0x22 /* S-NSSAI 9.11.2.8  */
+#define IEI_ALWAYSON_PDU    0x80 /* Always-on PDU session indication 9.11.4.3 */
+#define IEI_MAPPED_EPS      0x75 /* Mapped EPS bearer contexts 9.11.4.8  */
+#define IEI_EAP_MSG         0x78 /* EAP message 9.11.2.2  */
+#define IEI_AUTH_QOS_DESC   0x79 /* QoS flow descriptions 9.11.4.12 */
+#define IEI_EXT_CONF_OPT    0x7b /* Extended protocol configuration options 9.11.4.6  */
+#define IEI_DNN             0x25 /* DNN 9.11.2.1B  */
+
 #endif
