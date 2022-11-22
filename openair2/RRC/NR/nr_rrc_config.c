9bd08a1ddd90ae1fa6e3542faf022e01d9e980e5
francescomani <email@francescomani.it>
2022-11-22 13:54:20 +0100

    bugfix in SRS configuration

--- openair2/RRC/NR/nr_rrc_config.c
+++ openair2/RRC/NR/nr_rrc_config.c
@@ -477,7 +477,7 @@ void config_srs(NR_SetupRelease_SRS_Config_t *setup_release_srs_Config,
     srs_res0->resourceType.present = NR_SRS_Resource__resourceType_PR_periodic;
     srs_res0->resourceType.choice.periodic = calloc(1,sizeof(*srs_res0->resourceType.choice.periodic));
     srs_res0->resourceType.choice.periodic->periodicityAndOffset_p.present = NR_SRS_PeriodicityAndOffset_PR_sl160;
-    srs_res0->resourceType.choice.periodic->periodicityAndOffset_p.choice.sl160 = 17 + (uid>1)*10; // 17/17/.../147/157 are mixed slots
+    srs_res0->resourceType.choice.periodic->periodicityAndOffset_p.choice.sl160 = 17 + (uid >> 1) * 10; // 17/17/.../147/157 are mixed slots
   } else {
     srs_res0->resourceType.present = NR_SRS_Resource__resourceType_PR_aperiodic;
     srs_res0->resourceType.choice.aperiodic = calloc(1,sizeof(*srs_res0->resourceType.choice.aperiodic));
