c9b043eb09baffa60b26b6b10ea941ced657fbd1
Robert Schmidt <robert.schmidt@openairinterface.org>
2023-02-22 09:35:50 +0100

    Fix: Correct check thread pool initialized
    traceFd of thread pool can be:
    * -1: no tracing
    * valid file descriptor/>0: tracing

--- common/utils/threadPool/thread-pool.h
+++ common/utils/threadPool/thread-pool.h
@@ -286,7 +286,7 @@ static inline notifiedFIFO_elt_t *pullTpool(notifiedFIFO_t *responseFifo, tpool_
   notifiedFIFO_elt_t *msg= pullNotifiedFIFO(responseFifo);
   if (msg == NULL)
     return NULL;
-  AssertFatal(t->traceFd > 0, "Thread pool used while not initialized");
+  AssertFatal(t->traceFd != 0, "Thread pool used while not initialized");
   if (t->measurePerf)
     msg->returnTime=rdtsc_oai();
 
@@ -300,7 +300,7 @@ static inline notifiedFIFO_elt_t *pullTpool(notifiedFIFO_t *responseFifo, tpool_
 
 static inline notifiedFIFO_elt_t *tryPullTpool(notifiedFIFO_t *responseFifo, tpool_t *t) {
   notifiedFIFO_elt_t *msg= pollNotifiedFIFO(responseFifo);
-  AssertFatal(t->traceFd > 0, "Thread pool used while not initialized");
+  AssertFatal(t->traceFd != 0, "Thread pool used while not initialized");
   if (msg == NULL)
     return NULL;
 
