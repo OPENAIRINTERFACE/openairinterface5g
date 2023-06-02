537b721b4f9a25dc877a3a87d185be22e25a72fc
Robert Schmidt <robert.schmidt@openairinterface.org>
2023-06-02 14:14:48 +0200

    LTE UE entry script: print both configs if present

--- docker/scripts/lte_ue_entrypoint.sh
+++ docker/scripts/lte_ue_entrypoint.sh
@@ -3,21 +3,31 @@
 set -uo pipefail
 
 PREFIX=/opt/oai-lte-ue
-CONFIGFILE=$PREFIX/etc/ue_usim.conf
+USIM_CONFIGFILE=$PREFIX/etc/ue_usim.conf
 
-if [ ! -f $CONFIGFILE ]; then
-  echo "No configuration file found: please mount at $CONFIGFILE"
+if [ ! -f $USIM_CONFIGFILE ]; then
+  echo "No ue_usim.conf configuration file found: please mount at $USIM_CONFIGFILE"
   exit 255
 fi
 
 echo "=================================="
-echo "== Configuration file:"
-cat $CONFIGFILE
+echo "== USIM Configuration file:"
+cat $USIM_CONFIGFILE
 
 #now generate USIM files
 # At this point all operations will be run from $PREFIX!
 cd $PREFIX
-$PREFIX/bin/conf2uedata -c $CONFIGFILE -o $PREFIX
+$PREFIX/bin/conf2uedata -c $USIM_CONFIGFILE -o $PREFIX
+
+CONFIGFILE=$PREFIX/etc/ue.conf
+
+if [ ! -f $CONFIGFILE ]; then
+  echo "No ue.conf configuration file found"
+else
+  echo "=================================="
+  echo "== UE Configuration file:"
+  cat $CONFIGFILE
+fi
 
 # Load the USRP binaries
 echo "=================================="
@@ -36,10 +46,9 @@ while [[ $# -gt 0 ]]; do
   new_args+=("$1")
   shift
 done
-FILE=$PREFIX/etc/ue.conf
 if [[ -f "$FILE"  ]]; then
   new_args+=("-O")
-  new_args+=("$PREFIX/etc/ue.conf")
+  new_args+=("$CONFIGFILE")
 fi
 
 # enable printing of stack traces on assert
