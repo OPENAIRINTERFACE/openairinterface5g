bdc445f766638bd30a349c6e790ef4009f4458fc
francescomani <email@francescomani.it>
2023-06-20 18:50:50 +0200

    bugfix in RAR time allocation at UE

--- openair2/LAYER2/NR_MAC_UE/nr_ue_procedures.c
+++ openair2/LAYER2/NR_MAC_UE/nr_ue_procedures.c
@@ -4083,7 +4083,7 @@ int nr_ue_process_rar(nr_downlink_indication_t *dl_info, int pdu_id)
     // MCS
     rar_grant.mcs = (unsigned char) (rar->UL_GRANT_4 >> 4);
     // time alloc
-    rar_grant.Msg3_t_alloc = (unsigned char) (rar->UL_GRANT_3 & 0x07);
+    rar_grant.Msg3_t_alloc = (unsigned char) (rar->UL_GRANT_3 & 0x0f);
     // frequency alloc
     rar_grant.Msg3_f_alloc = (uint16_t) ((rar->UL_GRANT_3 >> 4) | (rar->UL_GRANT_2 << 4) | ((rar->UL_GRANT_1 & 0x03) << 12));
     // frequency hopping
