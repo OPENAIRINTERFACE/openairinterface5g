aed0cfe2af9d7926d4dad08693ff20164439a12b
Daniel Andrade <dandrade@allbesmart.pt>
2023-06-23 14:31:43 +0100

    Bugfix for getting band and numerology from command line for OAI UE

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -513,7 +513,7 @@ int main( int argc, char **argv ) {
       NR_UE_MAC_INST_t *mac = get_mac_inst(0);
 
       if (get_softmodem_params()->sa) { // set frame config to initial values from command line and assume that the SSB is centered on the grid
-        uint16_t nr_band = get_band(downlink_frequency[CC_id][0],uplink_frequency_offset[CC_id][0]);
+        uint16_t nr_band = get_softmodem_params()->band;
         mac->nr_band = nr_band;
         nr_init_frame_parms_ue_sa(&UE[CC_id]->frame_parms,
                                   downlink_frequency[CC_id][0],
