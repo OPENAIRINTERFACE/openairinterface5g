282a20578f84485e44cb4180dc9c0dad401452c1
Cedric Roux <cedric.roux@eurecom.fr>
2023-09-05 21:55:15 +0200

    nr rlc test: minor: change the logging

--- openair2/LAYER2/nr_rlc/tests/LOG/log.h
+++ openair2/LAYER2/nr_rlc/tests/LOG/log.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-#define LOG_E(x, ...) printf(__VA_ARGS__)
-#define LOG_D(x, ...) printf(__VA_ARGS__)
-#define LOG_W(x, ...) printf(__VA_ARGS__)
+#define LOG_E(x, ...) do { printf("LOG_E: "); printf(__VA_ARGS__); } while (0)
+#define LOG_D(x, ...) do { printf("LOG_D: "); printf(__VA_ARGS__); } while (0)
+#define LOG_W(x, ...) do { printf("LOG_W: "); printf(__VA_ARGS__); } while (0)
 
 #endif /* _NR_RLC_TESTS_LOG_H_ */
