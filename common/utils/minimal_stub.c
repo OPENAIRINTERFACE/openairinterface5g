f955a0fce2cb50bd60390b3014c0c4ac38b495cd
Robert Schmidt <robert.schmidt@openairinterface.org>
2023-11-23 18:26:21 +0100

    Declare uniqCfg in minimal_stub.c: required in log.c

--- common/utils/minimal_stub.c
+++ common/utils/minimal_stub.c
@@ -4,6 +4,9 @@
 int T_stdout;
 #endif
 
+struct configmodule_interface_s;
+struct configmodule_interface_s *uniqCfg = NULL;
+
 void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
 {
   if (assert) {
