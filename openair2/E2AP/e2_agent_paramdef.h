56843fc1d72f6bfb718459996d0e4471c1bf7440
Teodora <teodora.vladic@openairinterface.org>
2024-01-17 15:51:52 +0100

    Add E2 agent in CU-UP

--- openair2/E2AP/e2_agent_paramdef.h
+++ openair2/E2AP/e2_agent_paramdef.h
@@ -19,22 +19,24 @@
  *      contact@openairinterface.org
  */
 
-#ifndef E2_AGENT_ARGS_H
-#define E2_AGENT_ARGS_H
+#ifndef E2_AGENT_PARAMDEF_H_
+#define E2_AGENT_PARAMDEF_H_
 
-#include <stdint.h>
-#include <stdbool.h>
-#include <stddef.h>
+/* E2 Agent configuration */
+#define CONFIG_STRING_E2AGENT "e2_agent"
 
-// Wrapper for OAI
-typedef struct{
-  const char *ip;
-  const char *sm_dir;
-  const bool enabled;
-} e2_agent_args_t;
+#define E2AGENT_CONFIG_IP    "near_ric_ip_addr"
+#define E2AGENT_CONFIG_SMDIR "sm_dir"
 
-e2_agent_args_t RCconfig_NR_E2agent(void);
+static const char* const e2agent_config_ip_default = "127.0.0.1";
+static const char* const e2agent_config_smdir_default = ".";
 
-#endif
+#define E2AGENT_PARAMS_DESC { \
+  {E2AGENT_CONFIG_IP,    "RIC IP address",             0, strptr:NULL, defstrval:(char*)e2agent_config_ip_default,    TYPE_STRING, 0}, \
+  {E2AGENT_CONFIG_SMDIR, "Directory with SMs to load", 0, strptr:NULL, defstrval:(char*)e2agent_config_smdir_default, TYPE_STRING, 0}, \
+}
 
+#define E2AGENT_CONFIG_IP_IDX    0
+#define E2AGENT_CONFIG_SMDIR_IDX 1
 
+#endif 
--- openair2/GNB_APP/gnb_config.c
+++ openair2/GNB_APP/gnb_config.c
@@ -2266,33 +2266,3 @@ ngran_node_t get_node_type(void)
     return ngran_gNB;
   }
 }
-
-#ifdef E2_AGENT
-
-e2_agent_args_t RCconfig_NR_E2agent(void)
-{
-  paramdef_t e2agent_params[] = E2AGENT_PARAMS_DESC;
-  int ret = config_get(config_get_if(), e2agent_params, sizeofArray(e2agent_params), CONFIG_STRING_E2AGENT);
-  if (ret < 0) {
-    LOG_W(GNB_APP, "configuration file does not contain a \"%s\" section, applying default parameters from FlexRIC\n", CONFIG_STRING_E2AGENT);
-    return (e2_agent_args_t) { 0 };
-  }
-
-  bool enabled = config_isparamset(e2agent_params, E2AGENT_CONFIG_SMDIR_IDX)
-              && config_isparamset(e2agent_params, E2AGENT_CONFIG_IP_IDX);
-  e2_agent_args_t dst = {.enabled = enabled};
-  if (!enabled) {
-    LOG_W(GNB_APP, "E2 agent is DISABLED (for activation, define .%s.{%s,%s} parameters)\n", CONFIG_STRING_E2AGENT, E2AGENT_CONFIG_IP, E2AGENT_CONFIG_SMDIR);
-    return dst;
-  }
-
-  if (e2agent_params[E2AGENT_CONFIG_SMDIR_IDX].strptr != NULL)
-    dst.sm_dir = *e2agent_params[E2AGENT_CONFIG_SMDIR_IDX].strptr;
-
-  if (e2agent_params[E2AGENT_CONFIG_IP_IDX].strptr != NULL)
-    dst.ip = *e2agent_params[E2AGENT_CONFIG_IP_IDX].strptr;
-
-  return dst;
-}
-
-#endif // E2_AGENT
