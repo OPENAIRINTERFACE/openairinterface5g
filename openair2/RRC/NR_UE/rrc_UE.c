a0224e8faf064e45f06ae5a32c8b35bde5f3ca08
francescomani <email@francescomani.it>
2024-02-12 00:51:11 +0100

    assert on measurement gaps

--- openair2/RRC/NR_UE/rrc_UE.c
+++ openair2/RRC/NR_UE/rrc_UE.c
@@ -1285,13 +1285,8 @@ static void nr_rrc_ue_process_measConfig(rrcPerNB_t *rrc, NR_MeasConfig_t *const
   if (measConfig->measIdToAddModList)
     handle_measid_addmod(rrc, measConfig->measIdToAddModList, timers);
 
-  if (measConfig->measGapConfig != NULL) {
-    if (rrc->measGapConfig) {
-      memcpy(rrc->measGapConfig, (char *)measConfig->measGapConfig, sizeof(NR_MeasGapConfig_t));
-    } else {
-      rrc->measGapConfig = measConfig->measGapConfig;
-    }
-  }
+  AssertFatal(!measConfig->measGapConfig, "Measurement gaps not yet supported\n");
+  AssertFatal(!measConfig->measGapSharingConfig, "Measurement gaps not yet supported\n");
 
   if (measConfig->s_MeasureConfig) {
     if (measConfig->s_MeasureConfig->present == NR_MeasConfig__s_MeasureConfig_PR_ssb_RSRP) {
