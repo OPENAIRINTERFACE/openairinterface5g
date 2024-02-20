71790ccbdf840e55f50ddf014e9461527d8aadd3
francescomani <email@francescomani.it>
2024-02-20 15:58:48 +0100

    remove noS1 condition to init pdcp in phy-test mode

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -515,14 +515,11 @@ int main(int argc, char **argv)
   uint16_t node_number = get_softmodem_params()->node_number;
   ue_id_g = (node_number == 0) ? 0 : node_number - 2;
   AssertFatal(ue_id_g >= 0, "UE id is expected to be nonnegative.\n");
-  if(IS_SOFTMODEM_NOS1 || get_softmodem_params()->sa || get_softmodem_params()->nsa) {
-    if(node_number == 0) {
-      init_pdcp(0);
-    }
-    else {
-      init_pdcp(mode_offset + ue_id_g);
-    }
-  }
+
+  if(node_number == 0)
+    init_pdcp(0);
+  else
+    init_pdcp(mode_offset + ue_id_g);
 
   init_NR_UE(NB_UE_INST, uecap_file, reconfig_file, rbconfig_file);
 
